f7f45d8a85e09b5d25c379ec3e1d92e3978097c5
Greg Venech <greg.venech@gmail.com>
2016-06-25 19:57:51 -0400

    Adding a Container component

---
+++ src/components/container/container.js
@@ -0,0 +1,17 @@
+// Import Dependencies
+import React from 'react'
+
+// Load Styling
+import './container-style'
+
+// Create the "Container" component
+let Container = props => {
+	return (
+		<div className={ `container ${props.className || ''}`}>
+			{ props.children }
+		</div>
+	)
+}
+
+// Export it
+export default Container
