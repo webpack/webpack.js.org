df8bbde72e8bb52ba2bb91a11f026d36344a727c
Greg Venech <greg.venech@gmail.com>
2016-06-26 09:11:56 -0400

    Adding the initial entry point ("npm run build" or "dev-server" can now be run without issue)

---
+++ src/app.js
@@ -0,0 +1,14 @@
+// Import Dependencies
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// Import Components
+import Splash from 'Components/splash/splash'
+
+// Load Base Styling
+import 'Utilities/scss/reset'
+
+// 
+ReactDOM.render((
+	<Splash />
+), document.getElementById('root'))
