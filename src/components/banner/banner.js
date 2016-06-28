2c223bbc6d6399b7f1b58ca4ac1cc2dfbf080bbb
Greg Venech <greg.venech@gmail.com>
2016-06-28 01:47:16 -0400

    Reformatting and updating the banner

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -2,6 +2,7 @@
 import React from 'react'
 
 // Import Components
+import { Link } from 'react-router'
 import Container from 'Components/container/container'
 import Logo from 'Components/logo/logo'
 
@@ -10,13 +11,21 @@ import './banner-style'
 
 // Create the Banner component
 let Banner = props => {
-	return (
-		<header className="banner">
-			<Container className="banner-inner">
-				<Logo theme="light" />
-			</Container>
-		</header>
-	)
+    return (
+        <header className="banner">
+            <Container className="banner-inner">
+                <Logo theme="light" />
+
+                <nav className="banner-nav">
+                    <Link className="banner-nav-link" activeClassName="-active" to={{ pathname: '/guides' }}>Guides</Link>
+                    <Link className="banner-nav-link" activeClassName="-active" to={{ pathname: '/reference' }}>Reference</Link>
+                    <Link className="banner-nav-link" activeClassName="-active" to={{ pathname: '/contribute' }}>Contribute</Link>
+                    <Link className="banner-nav-link" activeClassName="-active" to={{ pathname: '/analyze' }}>Analyze</Link>
+                    <Link className="banner-nav-link" activeClassName="-active" to={{ pathname: '/donate' }}>Donate</Link>
+                </nav>
+            </Container>
+        </header>
+    )
 }
 
 // Export it
