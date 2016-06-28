99f20c3897c7011430bfa81e52cf3a517c52b93b
Greg Venech <greg.venech@gmail.com>
2016-06-28 01:50:34 -0400

    [fix] Adding an empty Footer component (should have committed this prior to 51bb660659ae4d40883c1067b9089cb257aa3f3e)

---
+++ src/components/footer/footer.js
@@ -0,0 +1,24 @@
+// Import Dependencies
+import React from 'react'
+
+// Import Components
+import { Link } from 'react-router'
+import Icon from 'Components/icon/icon'
+import Container from 'Components/container/container'
+
+// Load Styling
+import './footer-style'
+
+// Create the "Footer" component
+let Footer = props => {
+    return (
+        <footer className="footer">
+            <Container className="footer-inner">
+                
+            </Container>
+        </footer>
+    )
+}
+
+// Export it
+export default Footer
