8b4a989bfd34a746bfcb34a1f81dbe61f3dc766d
Greg Venech <greg.venech@gmail.com>
2016-06-29 00:04:16 -0400

    Adding a component for the Lost/404 page (currently just a placeholder)

---
+++ src/components/lost/lost.js
@@ -0,0 +1,19 @@
+// Import Dependencies
+import React, { Component } from 'react'
+
+// Import Components
+
+
+// Load Styling
+import './lost-style'
+
+// Export the "Lost" component
+export default class Lost extends Component {
+    render() {
+        return (
+            <main className="lost">
+                <span>Page not found</span>
+            </main>
+        )
+    }
+}
