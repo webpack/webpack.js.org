89a91d7993446746074eff61348e3f6f00dba692
Greg Venech <greg.venech@gmail.com>
2016-06-29 00:01:23 -0400

    Adding a placeholder component for the the Analyze page

---
+++ src/components/analyze/analyze.js
@@ -0,0 +1,19 @@
+// Import Dependencies
+import React, { Component } from 'react'
+
+// Import Components
+
+
+// Load Styling
+import './analyze-style'
+
+// Export the "Analyze" component
+export default class Analyze extends Component {
+    render() {
+        return (
+            <main className="analyze">
+                <span>Analyze Content</span>
+            </main>
+        )
+    }
+}
