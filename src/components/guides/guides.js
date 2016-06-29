f3d4b8603d1b375f670d0e9e20e49827ac845dd5
Greg Venech <greg.venech@gmail.com>
2016-06-29 00:03:27 -0400

    Adding a component for the Guides page (currently just a placeholder)

---
+++ src/components/guides/guides.js
@@ -0,0 +1,19 @@
+// Import Dependencies
+import React, { Component } from 'react'
+
+// Import Components
+
+
+// Load Styling
+import './guides-style'
+
+// Export the "Guides" component
+export default class Guides extends Component {
+    render() {
+        return (
+            <main className="guides">
+                <span>Guides Content</span>
+            </main>
+        )
+    }
+}
