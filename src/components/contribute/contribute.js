907400ddb570e1e97914bd6e571ad404cc29fdce
Greg Venech <greg.venech@gmail.com>
2016-06-29 00:02:41 -0400

    Adding a component for the Contribute page (currently just a placeholder)

---
+++ src/components/contribute/contribute.js
@@ -0,0 +1,19 @@
+// Import Dependencies
+import React, { Component } from 'react'
+
+// Import Components
+
+
+// Load Styling
+import './contribute-style'
+
+// Export the "Contribute" component
+export default class Contribute extends Component {
+    render() {
+        return (
+            <main className="contribute">
+                <span>Contribute Content</span>
+            </main>
+        )
+    }
+}
