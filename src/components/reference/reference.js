b3f68a0e0ea7dec25a25fdd6940e97d9b3c9c5cc
Greg Venech <greg.venech@gmail.com>
2016-06-29 00:07:47 -0400

    Adding a component for the Reference/Documentation page (currently just a placeholder)

---
+++ src/components/reference/reference.js
@@ -0,0 +1,19 @@
+// Import Dependencies
+import React, { Component } from 'react'
+
+// Import Components
+
+
+// Load Styling
+import './reference-style'
+
+// Export the "Reference" component
+export default class Reference extends Component {
+    render() {
+        return (
+            <main className="reference">
+                <span>Reference Content</span>
+            </main>
+        )
+    }
+}
