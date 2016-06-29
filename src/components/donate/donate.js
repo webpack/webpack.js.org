0d097a832fbe0fcbd0867159b0f178fe1c4624ba
Greg Venech <greg.venech@gmail.com>
2016-06-29 00:03:09 -0400

    Adding a component for the Donate page (currently just a placeholder)

---
+++ src/components/donate/donate.js
@@ -0,0 +1,19 @@
+// Import Dependencies
+import React, { Component } from 'react'
+
+// Import Components
+
+
+// Load Styling
+import './donate-style'
+
+// Export the "Donate" component
+export default class Donate extends Component {
+    render() {
+        return (
+            <main className="donate">
+                <span>Donate Content</span>
+            </main>
+        )
+    }
+}
