83d3c633c40a1077fad2819d2ce1e5fccc9f0949
Greg Venech <greg.venech@gmail.com>
2016-06-29 00:09:37 -0400

    Updating the Footer component

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -14,7 +14,23 @@ let Footer = props => {
     return (
         <footer className="footer">
             <Container className="footer-inner">
-                
+                <section className="footer-inner-left">
+                    <Link className="footer-inner-link" to={{ pathname: '/license' }}>License</Link>
+                    <a className="footer-inner-link" href="https://github.com/webpack/webpack/graphs/contributors">Contributors</a>
+                    <a className="footer-inner-link" href="https://github.com/webpack/webpack/issues">Issues</a>
+                </section>
+
+                <section className="footer-inner-middle">
+                    <Link to={{ pathname: '/' }}>
+                        <Icon depth={ 18 } hover />
+                    </Link>
+                </section>
+
+                <section className="footer-inner-right">
+                    <Link className="footer-inner-link" to={{ pathname: '/about' }}>About</Link>
+                    <Link className="footer-inner-link" to={{ pathname: '/resources' }}>Resources</Link>
+                    <Link className="footer-inner-link" to={{ pathname: '/donate' }}>Donate</Link>
+                </section>
             </Container>
         </footer>
     )
