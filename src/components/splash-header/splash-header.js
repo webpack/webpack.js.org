0991ae881c48b78080cccfbc8dcf4c88c1e2fe0a
Greg Venech <greg.venech@gmail.com>
2016-06-30 22:31:52 -0400

    Adding the SplashHeader component

---
+++ src/components/splash-header/splash-header.js
@@ -0,0 +1,16 @@
+// Import Dependencies
+import React, { Component } from 'react'
+
+// Load Styling
+import './splash-header-style'
+
+// Export the "SplashHeader" component
+export default class SplashHeader extends Component {
+    render() {
+        return (
+            <section className="splash-header">
+
+            </section>
+        )
+    }
+}
