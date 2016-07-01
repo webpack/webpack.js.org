52c1d188f2b32b9cc3bdb1f77d77bee78e6ca25c
Greg Venech <greg.venech@gmail.com>
2016-06-30 22:32:40 -0400

    Adding SplashHeader to splash and clearing temporary styles

--- src/components/splash/splash.js
+++ src/components/splash/splash.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react'
 
 // Import Components
-
+import SplashHeader from 'Components/splash-header/splash-header'
 
 // Load Styling
 import './splash-style'
@@ -12,7 +12,8 @@ export default class Splash extends Component {
     render() {
         return (
             <main className="splash">
-                <span>Splash Content</span>
+                <SplashHeader />
+                
             </main>
         )
     }
