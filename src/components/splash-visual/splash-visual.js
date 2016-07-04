dfe29191b6f1d4f4976c2e978131b0ec9aeb9c6a
Greg Venech <greg.venech@gmail.com>
2016-07-04 12:14:30 -0400

    Adding the SplashVisual component (currently empty)

---
+++ src/components/splash-visual/splash-visual.js
@@ -0,0 +1,16 @@
+// Import Dependencies
+import React, { Component } from 'react'
+
+// Load Styling
+import './splash-visual-style'
+
+// Export the "SplashVisual" component
+export default class SplashVisual extends Component {
+    render() {
+        return (
+            <div className="splash-visual">
+
+            </div>
+        )
+    }
+}
