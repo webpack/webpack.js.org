8382e53eac85372cc48f69b3fe9f6dd0d3a371e6
Greg Venech <greg.venech@gmail.com>
2016-07-04 12:18:15 -0400

    Using all the new Splash* components on the Splash page

--- src/components/splash/splash.js
+++ src/components/splash/splash.js
@@ -3,6 +3,10 @@ import React, { Component } from 'react'
 
 // Import Components
 import SplashHeader from 'Components/splash-header/splash-header'
+import SplashSection from 'Components/splash-section/splash-section'
+import SplashPhrase from 'Components/splash-phrase/splash-phrase'
+import SplashVisual from 'Components/splash-visual/splash-visual'
+import SplashIntro from 'Components/splash-intro/splash-intro'
 
 // Load Styling
 import './splash-style'
@@ -13,7 +17,18 @@ export default class Splash extends Component {
         return (
             <main className="splash">
                 <SplashHeader />
-                
+
+                <SplashSection theme="dark">
+                    <SplashPhrase />
+                </SplashSection>
+
+                <SplashSection theme="grey">
+                    <SplashVisual />
+                </SplashSection>
+
+                <SplashSection>
+                    <SplashIntro />
+                </SplashSection>
             </main>
         )
     }
