c810a8a493edfb9e2763a7aebc494fcc2d9fe52b
Juho Vepsalainen <bebraw@gmail.com>
2016-07-22 22:20:47 +0300

    antwar - Add the slash back to siteBase
    `base` needs this to work properly. Otherwise it resolves to the site
    before the last available slash...

--- antwar.config.js
+++ antwar.config.js
@@ -5,7 +5,7 @@ var highlight = require('./utilities/highlight');
 
 module.exports = {
   //assets: [] // custom assets to copy into the build
-  siteBase: '//webpack.github.io/webpack.io',
+  siteBase: '//webpack.github.io/webpack.io/',
   output: 'build',
   title: 'webpack',
   keywords: ['webpack', 'javascript', 'web development', 'programming'],
