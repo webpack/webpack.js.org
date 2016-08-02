b41cec2f5768ea0de1b6b69d523f9524c09d0ebc
Greg Venech <greg.venech@gmail.com>
2016-08-01 23:53:07 -0400

    Starting to lay out the Splash page

--- components/Splash.jsx
+++ components/Splash.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import Container from './Container';
+import Icon from './Icon';
 
 export default props => {
   let { page } = props;
 
   return (
     <div className="splash">
-      <div className="splash-visualization">
+      <section className="splash-viz">
+        <div className="splash-viz-modules">
 
-      </div>
+        </div>
 
-      <Container className="splash-content">
-        <h1>{ page.title }</h1>
-        <div dangerouslySetInnerHTML={{ __html: page.content }} />
-      </Container>
+        <div className="splash-viz-icon">
+          <Icon depth={ 150 } />
+        </div>
+
+        <div className="splash-viz-output">
+
+        </div>
+      </section>
+
+      <section className="splash-content">
+        <Container className="splash-content-inner">
+          <h1>{ page.title }</h1>
+          <div dangerouslySetInnerHTML={{ __html: page.content }} />
+        </Container>
+      </section>
     </div>
   );
 };
