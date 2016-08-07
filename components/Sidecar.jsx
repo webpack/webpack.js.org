55503129505140b1e824eb3cff749f6fc0e9abbc
Greg Venech <greg.venech@gmail.com>
2016-08-07 00:52:02 -0400

    Updating the Sidecar icon classes

--- components/Sidecar.jsx
+++ components/Sidecar.jsx
@@ -5,15 +5,15 @@ export default props => {
     <aside className="sidecar">
       <a href="https://github.com/webpack/webpack.io" className="sidecar-item -github">
         <span>See the Repo</span>
-        <i className="webpack-icon-github" />
+        <i className="icon-github" />
       </a>
       <a href="#" className="sidecar-item -gitter">
         <span>Get Help on Gitter</span>
-        <i className="webpack-icon-gitter" />
+        <i className="icon-gitter" />
       </a>
       <a href="#" className="sidecar-item -medium">
         <span>Visit Our Blog</span>
-        <i className="webpack-icon-medium" />
+        <i className="icon-medium" />
       </a>
     </aside>
   );
