5f88fcf87feb6459b70a914b865ed1ba2dff27bd
Greg Venech <greg.venech@gmail.com>
2016-08-06 23:57:53 -0400

    Updating the Sidecar with actual icons and titles

--- components/Sidecar.jsx
+++ components/Sidecar.jsx
@@ -4,13 +4,16 @@ export default props => {
   return (
     <aside className="sidecar">
       <a href="https://github.com/webpack/webpack.io" className="sidecar-item -github">
-        <i className="entypo-github" />
+        <span>See the Repo</span>
+        <i className="webpack-icon-github" />
       </a>
       <a href="#" className="sidecar-item -gitter">
-        <i className="entypo-gitter" />
+        <span>Get Help on Gitter</span>
+        <i className="webpack-icon-gitter" />
       </a>
       <a href="#" className="sidecar-item -medium">
-        <i className="entypo-medium" />
+        <span>Visit Our Blog</span>
+        <i className="webpack-icon-medium" />
       </a>
     </aside>
   );
