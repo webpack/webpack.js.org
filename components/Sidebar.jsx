d99be860e49e7e95b3f0d59e8cf98b8eb0eef722
Greg Venech <greg.venech@gmail.com>
2016-08-28 03:16:32 -0400

    Updating the Sidebar component to include sections within pages (still a WIP)

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import Link from './Link';
 
+// Just for testing
+let sections = [
+  { title: 'Overview', id: 'overview' },
+  { title: 'Usage', id: 'usage' },
+  { title: 'Options', id: 'options' },
+  { title: 'Multiple Targets', id: 'multiple-targets' }
+];
+
 export default props => {
   return (
     <nav className="sidebar">
       {
         props.pages.map((page, i) => (
-          <div key={ `sidebar-link-${i}` } className="sidebar-link">
-            <Link className="sidebar-link-text" to={ `/${page.url}` }>{ page.title }</Link>
+          <div className="sidebar-page" key={ `sidebar-section-${i}` }>
+            <span className="sidebar-page-title">
+              <Link to={ `/${page.url}` }>{ page.title }</Link>
+              <i className="sidebar-page-toggle icon-chevron-down" />
+            </span>
+
+            <ul className="sidebar-page-sections">
+              {
+                // Temporary for testing, should just be page.sections.map(...)
+                page.title === 'Targets' ? sections.map(section => (
+                  <li className="sidebar-page-section">
+                    <Link to={ `/${page.url}#${section.id}` }>{ section.title }</Link>
+                  </li>
+                )) : ''
+              }
+            </ul>
           </div>
         ))
       }
