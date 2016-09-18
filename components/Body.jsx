2324d2f6f9e23af50dcdedaa66e39545383305a6
Juho Vepsalainen <bebraw@gmail.com>
2016-09-18 21:05:27 +0300

    Update antwar to dev build
    It's running on dev version of Antwar now. Let me know of possible
    issues.
    
    Notes:
    
    * Development setup should be a little smoother now (esp. navigation)
    * It should build properly now (includes CSS link)
    
    The next bigger step is adding dynamic bits, but there's groundwork for
    that now.

--- components/Body.jsx
+++ components/Body.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Body from 'antwar-helpers/layouts/Body';
 import Navigation from './Navigation';
 import Sidecar from './Sidecar';
 import Footer from './Footer';
@@ -11,22 +10,15 @@ const pages = [
   { title: 'Github', url: '//github.com/webpack/webpack.js.org' }
 ];
 
-export default props => {
-  let { section, location, config } = props,
-      { pathname } = location,
-      { home = '/' } = config;
-
-  // XXX: drop this once there is root domain
-  if (home && __DEV__) { // eslint-disable-line no-undef
-    home = '/';
-  }
-
+const Body = ({ children }) => {
   return (
-    <Body { ...props }>
-      <Navigation home={ home } pages={ pages } />
+    <div>
+      <Navigation home="/" pages={ pages } />
         <Sidecar />
-        { props.children }
+        { children }
       <Footer />
-    </Body>
+    </div>
   );
 };
+
+export default Body;
