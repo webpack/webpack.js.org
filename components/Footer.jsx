5dde0da6bb8209c0a98146f469307acac3e9bd49
Greg Venech <greg.venech@gmail.com>
2016-09-27 00:06:58 -0400

    Updating the Footer to follow BEM

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,23 +6,23 @@ import Container from './Container';
 export default props => {
   return (
     <footer className="footer">
-      <Container className="footer-inner">
-        <section className="footer-inner-left">
-          <Link className="footer-inner-link" to="get-started">Get Started</Link>
-          <Link className="footer-inner-link" to="analyze">Analyze</Link>
-          <Link className="footer-inner-link" to="contribute">Contribute</Link>
+      <Container className="footer__inner">
+        <section className="footer__left">
+          <Link className="footer__link" to="get-started">Get Started</Link>
+          <Link className="footer__link" to="analyze">Analyze</Link>
+          <Link className="footer__link" to="contribute">Contribute</Link>
         </section>
 
-        <section className="footer-inner-middle">
+        <section className="footer__middle">
           <Link to={{ pathname: '/' }}>
             <Icon depth={ 18 } hover />
           </Link>
         </section>
 
-        <section className="footer-inner-right">
-          <Link className="footer-inner-link" to="//gitter.im/webpack/webpack">Support</Link>
-          <Link className="footer-inner-link" to="changelog">Changelog</Link>
-          <Link className="footer-inner-link" to="license">License</Link>
+        <section className="footer__right">
+          <Link className="footer__link" to="//gitter.im/webpack/webpack">Support</Link>
+          <Link className="footer__link" to="changelog">Changelog</Link>
+          <Link className="footer__link" to="license">License</Link>
         </section>
       </Container>
     </footer>
