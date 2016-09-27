b0537b946ea0747a5409f03cd716a58631c744a6
Greg Venech <greg.venech@gmail.com>
2016-09-27 00:19:45 -0400

    Cleaning up and updating the Sidebar to follow BEM

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,37 +1,30 @@
 import React from 'react';
 import Link from './Link';
 
-// Just for testing
-let sections = [
-  { title: 'Overview', id: 'overview' },
-  { title: 'Usage', id: 'usage' },
-  { title: 'Options', id: 'options' },
-  { title: 'Multiple Targets', id: 'multiple-targets' }
-];
-
 const Sidebar = ({ sectionName, pages }) => (
   <nav className="sidebar">
-    <SidebarPage url={`/${sectionName}`} title="Introduction" />
+    <Item url={ `/${sectionName}` } title="Introduction" />
+
     {
       pages.map(({ url, title }, i) =>
-        <SidebarPage key={`sidebar-page-${i}`} url={`/${url}`} title={title} />
+        <Item 
+          key={ `sidebar-item-${i}` } 
+          url={ `/${url}` } 
+          title={ title } />
       )
     }
   </nav>
 );
 
-const SidebarPage = ({ url, title }) => (
-  <div className="sidebar-page">
-    <span className="sidebar-page-title">
-      <Link to={url}>{title}</Link>
-      <i className="sidebar-page-toggle icon-chevron-down" />
-    </span>
-
-    <ul className="sidebar-page-sections">
+const Item = ({ url, title }) => (
+  <div className="sidebar-item">
+    <Link className="sidebar-item__title" to={ url }>{ title }</Link>
+    <i className="sidebar-item__toggle icon-chevron-down" />
+    <ul className="sidebar-item__sections">
       {/*
-        page.title === 'Targets' ? sections.map((section, j) => (
-          <li className="sidebar-page-section"
-              key={ `sidebar-page-${i}-section-${j}` }>
+        page.sections.map((section, j) => (
+          <li className="sidebar-item-section"
+              key={ `sidebar-item-${i}-section-${j}` }>
             <Link to={ `/${page.url}#${section.id}` }>{ section.title }</Link>
           </li>
         )) : ''
