4748fbefc06d298df74425935e25e801d80ae11d
Greg Venech <greg.venech@gmail.com>
2016-09-27 00:53:18 -0400

    Cleaning up and updating the Sidecar to follow BEM

--- components/Sidecar.jsx
+++ components/Sidecar.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
+import Link from './Link';
 
 export default props => {
   return (
     <aside className="sidecar">
-      <a href="https://github.com/webpack/webpack" className="sidecar-item -github">
-        <span>See the Repo</span>
-        <i className="icon-github" />
-      </a>
-      <a href="#" className="sidecar-item -gitter">
-        <span>Get Help on Gitter</span>
-        <i className="icon-gitter" />
-      </a>
-      <a href="https://medium.com/webpack" className="sidecar-item -medium">
-        <span>Visit Our Blog</span>
-        <i className="icon-medium" />
-      </a>
-      <a href="http://stackoverflow.com/questions/tagged/webpack" className="sidecar-item -so">
-        <span>Browse Webpack on SO</span>
-        <i className="icon-stack-overflow" />
-      </a>
+      <Link className="sidecar__link sidecar__link--github" to="//github.com/webpack/webpack">
+        <span className="sidecar__label">Fork the Repo</span>
+        <i className="sidecar__icon icon-github" />
+      </Link>
+      <Link className="sidecar__link sidecar__link--gitter" to="#">
+        <span className="sidecar__label">Find Help</span>
+        <i className="sidecar__icon icon-gitter" />
+      </Link>
+      <Link className="sidecar__link sidecar__link--medium" to="//medium.com/webpack">
+        <span className="sidecar__label">Visit Our Blog</span>
+        <i className="sidecar__icon icon-medium" />
+      </Link>
+      <Link className="sidecar__link sidecar__link--so" to="//stackoverflow.com/questions/tagged/webpack">
+        <span className="sidecar__label">Stack Overflow</span>
+        <i className="sidecar__icon icon-stack-overflow" />
+      </Link>
     </aside>
   );
 };
