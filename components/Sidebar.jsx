7d5f30610573c600d38d467f5778789b29758cfc
Greg Venech <greg.venech@gmail.com>
2016-10-02 23:33:33 -0400

    Updating the Sidebar to now render anchors and handle toggling

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,46 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Link from './Link';
 
-const Sidebar = ({ paths, sectionName, pages }) => {
-  // console.log('paths:', Object.keys(paths).map((key) => {
-  //   console.log(paths[key]);
-  //   return paths[key].title;
-  // }));
-
+const Sidebar = ({ sectionName, pages }) => {
   return (
     <nav className="sidebar">
       <div className="sidebar__inner">
-      <Item url={ `/${sectionName}` } title="Introduction" />
-      {
-        pages.map(({ url, title }, i) =>
-          <Item
-            key={ `sidebar-item-${i}` }
-            index={i}
-            url={ `/${url}` }
-            title={ title }
-          />
-        )
-      }
+        <Item url={ `/${sectionName}` } title="Introduction" />
+        {
+          pages.map(({ url, title, anchors }, i) =>
+            <Item
+              key={ `sidebar-item-${i}` }
+              index={i}
+              url={ `/${url}` }
+              title={ title }
+              anchors={ anchors }
+            />
+          )
+        }
       </div>
     </nav>
   );
 };
 
-const Item = ({ index, url, title }) => (
-  <div className="sidebar-item">
-    <Link className="sidebar-item__title" to={ url }>{ title }</Link>
-    <i className="sidebar-item__toggle icon-chevron-down" />
-    <ul className="sidebar-item__sections">
-      {/*
-       sections.map((section, j) => (
-       <li className="sidebar-item-section"
-       key={ `sidebar-item-${index}-section-${j}` }>
-       <Link to={ `/${url}#${section.id}` }>{ section.title }</Link>
-       </li>
-       ))*/
-      }
-    </ul>
-  </div>
-);
+class Item extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: false
+    };
+  }
+
+  render() {
+    let { index, url, title, anchors = [] } = this.props;
+    let emptyMod = !anchors.length ? 'sidebar-item--empty' : '';
+    let openMod = this.state.open ? 'sidebar-item--open' : '';
+    
+    return (
+      <div className={ `sidebar-item ${emptyMod} ${openMod}` }>
+        <Link className="sidebar-item__title" to={ url }>{ title }</Link>
+        <i className="sidebar-item__toggle icon-chevron-down"
+          onClick={ this.toggle.bind(this) } />
+        <ul className="sidebar-item__anchors">
+          {
+            anchors.map((anchor, j) => (
+              <li className="sidebar-item__anchor" key={ `anchor-${index}-${j}` }>
+                <Link to={ `${url}#${anchor.id}` }>{ anchor.title }</Link>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    );
+  }
+
+  toggle(e) {
+    this.setState({
+      open: !this.state.open
+    });
+  }
+}
 
 export default Sidebar;
