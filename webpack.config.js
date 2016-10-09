1d7ca2445d480d344037df142ef42196cf4642d7
Greg Venech <greg.venech@gmail.com>
2016-10-09 13:17:44 -0400

    Updating the config in preparation for file relocation

--- webpack.config.js
+++ webpack.config.js
@@ -4,153 +4,149 @@ var Autoprefixer = require('autoprefixer');
 var merge = require('webpack-merge');
 var webpack = require('webpack');
 
-module.exports = function(env) {
-  var cwd = process.cwd();
-  var stylePaths = [
-    path.join(cwd, 'styles', 'reset.css'),
-    path.join(cwd, 'styles', 'icons.css'),
-    path.join(cwd, 'styles', 'index.scss')
-  ];
+var cwd = process.cwd();
+var stylePaths = [
+  path.join(cwd, 'styles'),
+  path.join(cwd, 'components')
+];
 
-  switch(env) {
-    case 'start':
-      return merge(
-        commonConfig(stylePaths),
-        developmentConfig(stylePaths)
-      );
-    case 'interactive':
-      return merge(
-        commonConfig(stylePaths),
-        buildConfig(stylePaths),
-        interactiveConfig()
-      );
-    case 'build':
-      return merge(
-        commonConfig(stylePaths),
-        buildConfig(stylePaths)
-      );
+const commonConfig = {
+  entry: {
+    style: [
+      path.join(cwd, 'styles', 'index.scss')
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.scss']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel!eslint',
+        include: [
+          path.join(__dirname, 'components')
+        ]
+      },
+      {
+        test: /\.woff$/,
+        loaders: ['url?prefix=font/&limit=5000&mimetype=application/font-woff']
+      },
+      {
+        test: /\.ttf$|\.eot$/,
+        loaders: ['file?prefix=font/']
+      },
+      {
+        test: /\.jpg$/,
+        loaders: ['file']
+      },
+      {
+        test: /\.png$/,
+        loaders: ['file']
+      },
+      {
+        test: /\.svg$/,
+        loaders: ['raw']
+      },
+      {
+        test: /\.html$/,
+        loaders: ['raw']
+      }
+    ]
+  },
+  eslint: {
+    fix: true,
+    configFile: require.resolve('./.eslintrc')
+  },
+  postcss: function() {
+    return [ Autoprefixer ];
+  },
+  sassLoader: {
+    includePaths: [ path.join('./styles/partials') ]
   }
 };
 
-function commonConfig(stylePaths) {
-  return {
-    entry: {
-      style: stylePaths
-    },
-    resolve: {
-      extensions: ['', '.js', '.jsx', '.scss']
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.jsx?$/,
-          loader: 'babel!eslint',
-          include: [
-            path.join(__dirname, 'components')
-          ]
-        },
-        {
-          test: /\.woff$/,
-          loaders: ['url?prefix=font/&limit=5000&mimetype=application/font-woff']
-        },
-        {
-          test: /\.ttf$|\.eot$/,
-          loaders: ['file?prefix=font/']
-        },
-        {
-          test: /\.jpg$/,
-          loaders: ['file']
-        },
-        {
-          test: /\.png$/,
-          loaders: ['file']
-        },
-        {
-          test: /\.svg$/,
-          loaders: ['raw']
-        },
-        {
-          test: /\.html$/,
-          loaders: ['raw']
-        }
-      ]
-    },
-    eslint: {
-      fix: true,
-      configFile: require.resolve('./.eslintrc')
-    },
-    postcss: function() {
-      return [ Autoprefixer ];
+const interactiveConfig = {
+  resolve: {
+    alias: {
+      react: 'preact-compat',
+      'react-dom': 'preact-compat'
     }
-  };
-}
-
-function interactiveConfig() {
-  return {
-    resolve: {
-      alias: {
-        react: 'preact-compat',
-        'react-dom': 'preact-compat'
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
       }
-    },
-    plugins: [
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          warnings: false
-        }
-      })
+    })
+  ]
+};
+
+const developmentConfig = {
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: stylePaths
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'postcss', 'sass'],
+        include: stylePaths
+      }
     ]
-  };
-}
+  }
+};
 
-function developmentConfig(stylePaths) {
-  return {
-    module: {
-      loaders: [
-        {
-          test: /\.css$/,
-          loaders: ['style', 'css'],
-          include: stylePaths
-        },
-        {
-          test: /\.scss$/,
-          loaders: ['style', 'css', 'postcss', 'sass'],
-          include: stylePaths
-        }
-      ]
-    }
-  };
-}
+const buildConfig = {
+  output: {
+    publicPath: '/assets/'
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].css', {
+      allChunks: true
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract(
+          'style',
+          'css'
+        ),
+        include: stylePaths
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract(
+          'style',
+          'css!postcss!sass'
+        ),
+        include: stylePaths
+      }
+    ]
+  }
+};
 
-function buildConfig(stylePaths) {
-  return {
-    output: {
-      publicPath: '/assets/'
-    },
-    plugins: [
-      new ExtractTextPlugin('[name].css', {
-        allChunks: true
-      })
-    ],
-    module: {
-      loaders: [
-        {
-          test: /\.css$/,
-          loader: ExtractTextPlugin.extract(
-            'style',
-            'css'
-          ),
-          include: stylePaths
-        },
-        {
-          test: /\.scss$/,
-          loader: ExtractTextPlugin.extract(
-            'style',
-            'css!postcss!sass'
-          ),
-          include: stylePaths
-        }
-      ]
-    }
-  };
-}
+module.exports = function(env) {
+  switch(env) {
+    case 'start':
+      return merge(
+        commonConfig,
+        developmentConfig
+      );
+    case 'interactive':
+      return merge(
+        commonConfig,
+        buildConfig,
+        interactiveConfig
+      );
+    case 'build':
+      return merge(
+        commonConfig,
+        buildConfig
+      );
+  }
+};
