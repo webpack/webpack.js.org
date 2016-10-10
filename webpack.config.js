0b45e1519c9dd7dc2364e5cdbc907f8aebab267d
Greg Venech <greg.venech@gmail.com>
2016-10-10 01:28:03 -0400

    Cleaning up font directory, adding geomanist and cabin, and breaking fonts into a separate file

--- webpack.config.js
+++ webpack.config.js
@@ -29,12 +29,8 @@ const commonConfig = {
         ]
       },
       {
-        test: /\.woff$/,
+        test: /\.woff2?$/,
         loaders: ['url?prefix=font/&limit=5000&mimetype=application/font-woff']
-      },
-      {
-        test: /\.ttf$|\.eot$/,
-        loaders: ['file?prefix=font/']
       },
       {
         test: /\.jpg$/,
