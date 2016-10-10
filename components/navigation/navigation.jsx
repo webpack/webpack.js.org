1aa7afea7cfcc609b9e4b7ed743316f4db6ac3db
Greg Venech <greg.venech@gmail.com>
2016-10-10 01:37:44 -0400

    Adding react-headroom and updating the navigation component (@bebraw not sure if rh will play nicely with preact)

--- components/navigation/navigation.jsx
+++ components/navigation/navigation.jsx
@@ -1,34 +1,33 @@
 import React from 'react';
+import Headroom from 'react-headroom';
 import Link from '../link/link';
 import Container from '../container/container';
 import Logo from '../logo/logo';
 import './navigation-style';
 
 export default ({ home = '/', pages, onToggleNav }) => (
-  <div className="navigation">
-    <Container>
-      <div className="navigation__inner">
-        <button id="menu-btn" className="navigation__mobilebtn" onClick={onToggleNav}>
-          Open navigation
-        </button>
-        <Link className="navigation__logo" to="/">
-          <Logo theme="light" />
-        </Link>
+  <Headroom className="navigation">
+    <Container className="navigation__inner">
+      <button id="menu-btn" className="navigation__mobilebtn" onClick={onToggleNav}>
+        Open navigation
+      </button>
+      <Link className="navigation__logo" to="/">
+        <Logo theme="light" />
+      </Link>
 
-        <nav className="navigation__links">
-          {
-            pages.map((link, i) => (
-              <Link
-                key={ `navigation__link${i}` }
-                className="navigation__link"
-                activeClassName="navigation__link--active"
-                to={ `/${link.url}` }>
-                { link.title }
-              </Link>
-            ))
-          }
-        </nav>
-      </div>
+      <nav className="navigation__links">
+        {
+          pages.map((link, i) => (
+            <Link
+              key={ `navigation__link${i}` }
+              className="navigation__link"
+              activeClassName="navigation__link--active"
+              to={ `/${link.url}` }>
+              { link.title }
+            </Link>
+          ))
+        }
+      </nav>
     </Container>
-  </div>
+  </Headroom>
 );
