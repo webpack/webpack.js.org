99c60b07bfda9ac73d0eb505763988a414504fec
Greg Venech <greg.venech@gmail.com>
2016-10-11 22:30:40 -0400

    Refactoring Navigation to allow toggling of the Sidebar

--- components/navigation/navigation.jsx
+++ components/navigation/navigation.jsx
@@ -5,30 +5,42 @@ import Container from '../container/container';
 import LogoImg from './logo.png';
 import './navigation-style';
 
-export default ({ home = '/', pages, onToggleNav }) => (
-  <Headroom className="navigation">
-    <Container className="navigation__inner">
-      <button id="menu-btn" className="navigation__mobilebtn" onClick={onToggleNav}>
-        Open navigation
-      </button>
+export default class Navigation extends React.Component {
+  render() {
+    return (
+      <Headroom className="navigation">
+        <Container className="navigation__inner">
+          <div className="navigation__mobile" onClick={ this._toggleSidebar }>
+            <i className="icon-menu" />
+          </div>
 
-      <Link className="navigation__logo" to="/">
-        <img src={ LogoImg } />
-      </Link>
+          <Link className="navigation__logo" to="/">
+            <img src={ LogoImg } />
+          </Link>
 
-      <nav className="navigation__links">
-        {
-          pages.map((link, i) => (
-            <Link
-              key={ `navigation__link${i}` }
-              className="navigation__link"
-              activeClassName="navigation__link--active"
-              to={ `/${link.url}` }>
-              { link.title }
-            </Link>
-          ))
-        }
-      </nav>
-    </Container>
-  </Headroom>
-);
+          <nav className="navigation__links">
+            {
+              this.props.pages.map((link, i) => (
+                <Link
+                  key={ `navigation__link${i}` }
+                  className="navigation__link"
+                  activeClassName="navigation__link--active"
+                  to={ `/${link.url}` }>
+                  { link.title }
+                </Link>
+              ))
+            }
+          </nav>
+        </Container>
+      </Headroom>
+    );
+  }
+
+  _toggleSidebar(e) {
+    let sidebar = document.querySelector('.sidebar');
+    let modifier = 'sidebar--visible';
+
+    console.debug(sidebar.classList);
+    sidebar.classList.toggle(modifier);
+  }
+}
