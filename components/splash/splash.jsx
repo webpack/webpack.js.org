0befcee7227b168d0f505c74cfd5a146599778da
NejcZdovc <nejc.zdovc@3zsistemi.si>
2016-10-18 07:46:08 +0200

    Fixing svg limitation of opencollective

--- components/splash/splash.jsx
+++ components/splash/splash.jsx
@@ -3,6 +3,7 @@ import Interactive from 'antwar-interactive';
 import Container from '../container/container';
 import SplashViz from '../splash-viz/splash-viz';
 import './splash-style';
+import Support from '../support/support';
 
 export default props => {
   let { page } = props;
@@ -29,9 +30,9 @@ export default props => {
         <h1>Support the Team!</h1>
 
         <p>Through contributions, donations, and sponsorship, you allow webpack to thrive. Your donations directly support office hours, continued enhancements, and most importantly, great documentation and learning material!</p>
-        
-        <object type="image/svg+xml" data="https://opencollective.com/webpack/sponsors.svg"></object>
-        <object type="image/svg+xml" data="https://opencollective.com/webpack/backers.svg"></object>
+
+        <Support number={ 5 } type={"sponsor"} />
+        <Support number={ 20 } type={"backer"} />
       </Container>
     </div>
   );
