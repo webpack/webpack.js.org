6489294dd7c3fccc36069fb22f8e4ed49b4b6032
Greg Venech <greg.venech@gmail.com>
2016-10-18 18:10:14 -0400

    Fixing a console error and simplifying the Support component

--- components/support/support.jsx
+++ components/support/support.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
 
 export default ({number, type}) => {
-  let fun = 'sponsorLoaded';
-  let style = 'sponsors';
+  let functionName = 'sponsorLoaded';
 
- if(type === 'backer') {
-   fun = 'backerLoaded';
-   style = 'backers';
- }
+  if (type === 'backer') {
+    functionName = 'backerLoaded';
+  }
+
+  function getImage(a) {
+    if (typeof window !== 'undefined') {
+      window.CloudFlare && window.CloudFlare.push(
+        function(b) {
+          b(['cloudflare/rocket'], function(c) {
+            c.push(function() {
+              (function() { 
+                window.avatars[functionName]();
+              }).call(a);
+            });
+          });
+        }
+      );
+    }
+  }
 
   return (
-    <div className={style}>
+    <div className="support">
       {[...Array(number)].map((x, i) =>
         <a href={"https://opencollective.com/webpack/"+ type +"/" + i + "/website"} target="_blank" key={i}>
           <img src={"//opencollective.com/webpack/"+ type +"/" + i + "/avatar"} alt=""
-               onLoad={"(function(a){window.CloudFlare &amp;&amp; window.CloudFlare.push(function(b){b([&quot;cloudflare/rocket&quot;],function(c){c.push(function(){(function(){window.avatars." + fun + "()}).call(a)})})})})(this);"}
+            onLoad={ getImage(this) }
           />
         </a>
       )}
