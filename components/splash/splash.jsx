9d053a49bc2c2448603d96b8bf54afac42516364
Juho Vepsalainen <bebraw@gmail.com>
2016-10-23 13:37:59 +0300

    splash - Drop trailing space

--- components/splash/splash.jsx
+++ components/splash/splash.jsx
@@ -14,7 +14,7 @@ export default props => {
           <h1 id="opencollective-banner" className="splash__sponsorheadline">Help support webpack!</h1>
           <p>By your contributions, donations, and sponsorship, you allow webpack to thrive.</p>
           <p>Your donations go directly towards supporting office hours, continued enhancements, and most importantly, great documentation and learning material!</p>
-          
+
           <object type="image/svg+xml" data="https://opencollective.com/webpack/sponsors.svg"></object>
           <object type="image/svg+xml" data="https://opencollective.com/webpack/backers.svg"></object>
         </Container>
@@ -32,11 +32,11 @@ export default props => {
       <section className="splash__section">
         <Container className="splash__content">
           <h1>{ page.title }</h1>
-          <div dangerouslySetInnerHTML={{ 
-            __html: page.content 
+          <div dangerouslySetInnerHTML={{
+            __html: page.content
           }} />
         </Container>
-      </section>        
+      </section>
 
 
     </div>
