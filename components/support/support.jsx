ffef3eec2e5d6f053527f2a63621a6d96e0054dd
Greg Venech <greg.venech@gmail.com>
2016-10-24 23:54:48 -0400

    Cleaning up the support sections

--- components/support/support.jsx
+++ components/support/support.jsx
@@ -1,37 +1,44 @@
 import React from 'react';
+import './support-style';
+
+function getImage(a, funcName) {
+  if (typeof window !== 'undefined') {
+    window.CloudFlare && window.CloudFlare.push(
+      function(b) {
+        b(['cloudflare/rocket'], function(c) {
+          c.push(function() {
+            (function() { 
+              window.avatars[funcName]();
+            }).call(a);
+          });
+        });
+      }
+    );
+  }
+}
 
 export default ({number, type}) => {
-  let functionName = 'sponsorLoaded';
-
-  if (type === 'backer') {
-    functionName = 'backerLoaded';
-  }
-
-  function getImage(a) {
-    if (typeof window !== 'undefined') {
-      window.CloudFlare && window.CloudFlare.push(
-        function(b) {
-          b(['cloudflare/rocket'], function(c) {
-            c.push(function() {
-              (function() { 
-                window.avatars[functionName]();
-              }).call(a);
-            });
-          });
-        }
-      );
-    }
-  }
+  let funcName = type === 'backer' ? 'backerLoaded' : 'sponsorLoaded';
 
   return (
     <div className="support">
       {[...Array(number)].map((x, i) =>
-        <a href={"https://opencollective.com/webpack/"+ type +"/" + i + "/website"} target="_blank" key={i}>
-          <img src={"//opencollective.com/webpack/"+ type +"/" + i + "/avatar"} alt=""
-            onLoad={ getImage(this) }
-          />
+        <a key={ i }
+          className="support__item"
+          href={ `https://opencollective.com/webpack/${type}/${i}/website` } 
+          target="_blank">
+          <img 
+            src={ `//opencollective.com/webpack/${type}/${i}/avatar` } 
+            alt={ `${type} avatar` }
+            onLoad={ getImage(this, funcName) } />
         </a>
       )}
+
+      <div className="support__bottom">
+        <a className="support__button" href="https://opencollective.com/webpack#support">
+          Become a { type }
+        </a>
+      </div>
     </div>
   );
 };
