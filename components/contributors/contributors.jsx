8dff4cda872e8273a30d13111a15378e3b777067
Juho Vepsalainen <bebraw@gmail.com>
2016-10-26 09:49:49 +0300

    Don't render "Contributors" header if there aren't any

--- components/contributors/contributors.jsx
+++ components/contributors/contributors.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import './contributors-style';
 
 export default ({contributors}) => {
+  if (!contributors.length) {
+    return <noscript />;
+  }
+
   return (
     <div className="contributors">
       <h3>Contributors:</h3>
       <div className="contributors__list">
-        { 
-          (contributors || []).map((contributor) => (
-            <a key={ contributor } 
+        {
+          (contributors).map((contributor) => (
+            <a key={ contributor }
               className="contributor"
               href={ `https://github.com/${contributor}` }>
               <img src={ `https://github.com/${contributor}.png?size=50` } />
