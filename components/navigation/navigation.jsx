9732d939099cfb70db8ca1bfd294e77793593e62
Kees Kluskens <kees@webduck.nl>
2016-11-01 17:06:17 +0100

    Add some styling

--- components/navigation/navigation.jsx
+++ components/navigation/navigation.jsx
@@ -66,7 +66,7 @@ export default class Navigation extends React.Component {
               type="text" 
               className="navigation__search-input"
               ref={ ref => this.searchInput = ref }
-              placeholder="Search the docs..."
+              placeholder="Search documentation…"
               onFocus={ this._handleFocus.bind(this) }
               onChange={ this._handleSearch.bind(this) } />
             <button 
