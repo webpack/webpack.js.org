dd3a86679d67f7b1ec66ba212e2b797fdae87202
Kees Kluskens <kees@webduck.nl>
2016-11-20 13:23:14 +0100

    Fix footer icon with a hack

--- components/logo/logo.jsx
+++ components/logo/logo.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Logo from '../../assets/site-logo.svg';
+// TODO: Without this useless import, the icon in the footer breaks. Very weird.
+import '../../assets/icon-square-small.svg';
 import './logo-style';
 
 export default () => {
