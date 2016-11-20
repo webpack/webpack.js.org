4a34463257516b8641de6dbed8f95c084da56bf0
Kees Kluskens <kees@webduck.nl>
2016-11-20 12:37:47 +0100

    Use SVG logo with text

--- components/logo/logo.jsx
+++ components/logo/logo.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import Icon from '../../assets/icon-square-small.svg';
+import Icon from '../../assets/site-logo.svg';
 import './logo-style';
 
 export default props => {
   let { light } = props;
 
   return (
-    <span className={ `logo ${light ? 'logo--light' : ''}` }>
-      <img className="logo__icon" src={ Icon } />
-      <span className="logo__text">webpack</span>
-    </span>
-  );    
+    <img className="logo" src={ Icon } />
+  );
 };
