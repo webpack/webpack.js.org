412e99e263bc7fccc0c624384f43f76845f01af2
Sean Larkin <sean.larkin@cuw.edu>
2016-11-21 14:21:09 -0600

    feat(sidebar): add version in desktop sidebar

--- components/sidebar/sidebar.jsx
+++ components/sidebar/sidebar.jsx
@@ -3,17 +3,17 @@ import SidebarItem from '../sidebar-item/sidebar-item';
 import './sidebar-style';
 
 export default props => {
-  let { sectionName, pages, currentPage } = props;
+  let { sectionName, pages, currentPage, version } = props;
 
   return (
     <nav className="sidebar">
       <div className="sidebar__inner">
+        <h3 className="sidebar--item">Version 2.1</h3>
         <SidebarItem 
           url={ `/${sectionName}` } 
           title="Introduction"
           currentPage= { currentPage }
         />
-
         {
           pages.map(({ url, title, anchors }, i) =>
             <SidebarItem
