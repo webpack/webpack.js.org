bd02cd0fed6eb3b0815582bf52ebc9ae6471cb37
Fernando Montoya <montogeek@gmail.com>
2016-11-24 08:05:12 -0500

    Fix GitHub typo

--- components/vote/influence.jsx
+++ components/vote/influence.jsx
@@ -5,17 +5,17 @@ export default class InfluenceComponent extends React.Component {
     constructor(props) {
         super(props);
     }
-    
+
     render() {
-        return (this.props.type === "normal" ? ( 
+        return (this.props.type === "normal" ? (
             <section className="influence-info influence-info__section">
                 <h1 className="influence-info__header">What is Influence?</h1>
-                <p><em>Influence</em> is a unit of measure based on time you have been a member on github. However, in 2017 and on you will recieve one influence per day.</p>            
+                <p><em>Influence</em> is a unit of measure based on time you have been a member on GitHub. However, in 2017 and on you will recieve one influence per day.</p>
             </section>
         ) : (
             <section className="influence-info influence-info__section">
                 <h1 className="influence-info__header">What is Golden Influence?</h1>
-                <p><em>Golden Influence</em> is equal to 100 <i>normal influence</i>. Golden Influence is obtained by being a backer or sponsor on our <a href="https://opencollective.com/webpack">Open Collective page</a>.</p>            
+                <p><em>Golden Influence</em> is equal to 100 <i>normal influence</i>. Golden Influence is obtained by being a backer or sponsor on our <a href="https://opencollective.com/webpack">Open Collective page</a>.</p>
             </section>
         ));
     }
