1be5fff1963db21a3d05481fb9c4e3f0c615ec93
Greg Venech <greg.venech@gmail.com>
2016-12-04 10:38:52 -0500

    Refactoring Organization into a card view

--- components/organization/organization.jsx
+++ components/organization/organization.jsx
@@ -12,59 +12,54 @@ let Shield = props => (
 
 export default props => {
   return (
-    <Container className="organization">
-      <table>
-        <thead>
-          <tr>
-            <th>project</th>
-            <th>downloads/stars</th>
-            <th>activity</th>
-            <th>issues/prs</th>
-            <th>maintainer</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            Items.map(org => (
-              <tr key={ org.repo }>
-                <td>
-                  <a href={ `https://github.com/${org.repo}` }>{ org.repo }</a>
-                </td>
-                <td>
-                  <Shield content={ `npm/dm/${org.npm}`} label="npm" />
-                  &nbsp;
-                  <Shield content={ `github/stars/${org.repo}` } label="*" />
-                </td>
-                <td>
-                  <Shield 
-                    content={ `github/commits-since/${org.repo}/${encodeURIComponent("master@{6 months ago}")}` }
-                    label="6m" />
-                  &nbsp;
-                  <Shield 
-                    content={ `github/commits-since/${org.repo}/${encodeURIComponent("master@{3 months ago}")}` }
-                    label="3m" />
-                  &nbsp;
-                  <Shield 
-                    content={ `github/commits-since/${org.repo}/${encodeURIComponent("master@{1 month ago}")}` }
-                    label="1m" />
-                  &nbsp;
-                  <Shield 
-                    content={ `github/commits-since/${org.repo}/${encodeURIComponent("master@{1 week ago}")}` }
-                    label="1w" />
-                </td>
-                <td>
-                  <Shield content={ `github/issues-raw/${org.repo}` } label="issues" />
-                  &nbsp;
-                  <Shield content={ `github/issues-pr-raw/${org.repo}` } label="prs" />
-                </td>
-                <td>
-                  <Contributors contributors={[ org.maintainer ]} />
-                </td>
-              </tr>
-            ))
-          }
-        </tbody>
-      </table>
+    <Container className="organization page__content">
+      <h1>The Organization</h1>
+
+      <p>A list of all commonly used projects in the webpack ecosystem.</p>
+
+      <div className="organization__projects">
+        {
+          Items.map(org => (
+            <div className="organization__project" key={ org.repo }>
+              <a className="organization__title" href={ `https://github.com/${org.repo}` }>
+                <h4>{ org.repo }</h4>
+              </a>
+
+              <p>Short description...</p>
+
+              <h6>Downloads and Stars</h6>
+              <Shield content={ `npm/dm/${org.npm}`} label="npm" />
+              &nbsp;
+              <Shield content={ `github/stars/${org.repo}` } label="*" />
+              
+              <h6>Activity</h6>
+              <Shield 
+                content={ `github/commits-since/${org.repo}/${encodeURIComponent("master@{6 months ago}")}` }
+                label="6m" />
+              &nbsp;
+              <Shield 
+                content={ `github/commits-since/${org.repo}/${encodeURIComponent("master@{3 months ago}")}` }
+                label="3m" />
+              &nbsp;
+              <Shield 
+                content={ `github/commits-since/${org.repo}/${encodeURIComponent("master@{1 month ago}")}` }
+                label="1m" />
+              &nbsp;
+              <Shield 
+                content={ `github/commits-since/${org.repo}/${encodeURIComponent("master@{1 week ago}")}` }
+                label="1w" />
+              
+              <h6>Issues and PRs</h6>
+              <Shield content={ `github/issues-raw/${org.repo}` } label="issues" />
+              &nbsp;
+              <Shield content={ `github/issues-pr-raw/${org.repo}` } label="prs" />
+              
+              <h6>Maintainers</h6>
+              <Contributors contributors={[ org.maintainer ]} />
+            </div>
+          ))
+        }
+      </div>
     </Container>
   );
 };
