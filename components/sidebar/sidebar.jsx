26e6744729777539e28eae9ad9e3fd3ae45728d0
Rafael Rinaldi <rafael@rinaldi.io>
2016-12-14 17:02:51 -0500

    feat(sidebar): fixed sidebar on guides page

--- components/sidebar/sidebar.jsx
+++ components/sidebar/sidebar.jsx
@@ -1,31 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SidebarItem from '../sidebar-item/sidebar-item';
 
-export default props => {
-  let { sectionName, pages, currentPage } = props;
+export default class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {shouldFixSidebar: false};
+  }
 
-  return (
-    <nav className="sidebar">
-      <div className="sidebar__inner">
-        <h3 className="sidebar-item__version">Version 2.1</h3>
-        <SidebarItem
-          url={ `/${sectionName}` }
-          title="Introduction"
-          currentPage= { currentPage }
-        />
-        {
-          pages.map(({ url, title, anchors }, i) =>
-            <SidebarItem
-              key={ `sidebar-item-${i}` }
-              index={i}
-              url={ `/${url}` }
-              title={ title }
-              anchors={ anchors }
-              currentPage= { currentPage }
-            />
-          )
-        }
-      </div>
-    </nav>
-  );
-};
+  componentDidMount() {
+    document.addEventListener('scroll', this.onScroll.bind(this));
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('scroll', this.onScroll.bind(this));
+  }
+
+  onScroll() {
+    let { scrollY } = window;
+    let offsetY = document.querySelector('header').offsetHeight || 55;
+    let shouldFixSidebar = scrollY >= offsetY;
+
+    this.setState({shouldFixSidebar});
+  }
+
+  render() {
+    let { sectionName, pages, currentPage } = this.props;
+    let { shouldFixSidebar } = this.state;
+    let stickyStyle = {
+      position: 'fixed',
+      top: 0
+    };
+    let style = shouldFixSidebar ? stickyStyle : null;
+
+    return (
+      <nav className="sidebar" style={ style }>
+        <div className="sidebar__inner">
+          <h3 className="sidebar-item__version">Version 2.1</h3>
+          <SidebarItem
+            url={ `/${sectionName}` }
+            title="Introduction"
+            currentPage= { currentPage }
+          />
+          {
+            pages.map(({ url, title, anchors }, i) =>
+              <SidebarItem
+                key={ `sidebar-item-${i}` }
+                index={i}
+                url={ `/${url}` }
+                title={ title }
+                anchors={ anchors }
+                currentPage= { currentPage }
+              />
+            )
+          }
+        </div>
+      </nav>
+    );
+  }
+}
