40ecbe708675b86d84ed9fab01c8e55889cba0b0
lizhihua <275091674@qq.com>
2017-03-10 20:11:12 +0800

    meta description

--- antwar.config.js
+++ antwar.config.js
@@ -7,7 +7,7 @@ var highlight = require('./utilities/highlight');
 module.exports = {
   template: {
     title: 'webpack',
-    description: 'webpack is a module bundler. Its main purpose is to bundle JavaScript files for usage in a browser, yet it is also capable of transforming, bundling, or packaging just about any resource or asset.',
+    description: 'webpack 是一个模块打包器。它的主要目标是将 JavaScript 文件打包在一起，打包后的文件用于在浏览器中使用，但它也能够胜任转换(transform)、打包(bundle)或包裹(package)任何资源(resource or asset)。',
     file: path.join(__dirname, 'template.ejs')
   },
   output: 'build',
