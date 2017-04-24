075864d5a95759be77d3d0da32d9538f010452c1
Greg Venech <greg.venech@gmail.com>
2017-04-24 18:32:57 -0400

    fix(sidebar): remove html from anchor links and ids

--- utilities/markdown.js
+++ utilities/markdown.js
@@ -144,7 +144,7 @@ function parseContent(data) {
 }
 
 function parseAnchor(string) {
-  var stripped = string.replace(/\[(.+)\]\(.+\)/gi, '$1');
+  var stripped = string.replace(/\[(.+)\]\(.+\)/gi, '$1').replace(/(<([^>]+)>)/ig, '');
   var clean = stripped.replace(/`/g, '');
 
   return {
