d2e53241d2f791f52e92ac95b12c0256d5333dba
Greg Venech <greg.venech@gmail.com>
2017-04-27 01:10:17 -0400

    refactor(support): clean up support component and fetch actual json from opencollective
    Fetching the actual JSON containing our supporters gives us more flexibility
    to tackle issues like #411. It also give a model to follow for our, now extracted,
    additional sponsors list (those not from open collective).

--- components/support/support.jsx
+++ components/support/support.jsx
@@ -1,56 +1,59 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Additional from './support-additional.json';
+import 'whatwg-fetch';
 import './support-style';
 
-const MISSING_SPONSORS = [
-  {
-    index: 33,
-    name: 'MoonMail',
-    title: 'Email Marketing Software',
-    logo: 'https://static.moonmail.io/moonmail-logo.svg',
-    url: 'https://moonmail.io/?utm_source=webpack.js.org'
-  },
-  {
-    index: 34,
-    name: 'MONEI',
-    title: 'Best payment gateway rates',
-    logo: 'https://static.monei.net/monei-logo.svg',
-    url: 'https://monei.net/?utm_source=webpack.js.org'
-  }
-];
+export default class Support extends React.Component {
+  static propTypes = {
+    number: PropTypes.number.isRequired,
+    type: PropTypes.oneOf([
+      'sponsors',
+      'backers'
+    ]).isRequired
+  };
 
-export default ({number, type}) => {
-  const supportItems = [...Array(number)].map((x, i) =>
-    <a key={ i }
-       className="support__item"
-       href={ `https://opencollective.com/webpack/${type}/${i}/website?requireActive=false` }
-       target="_blank">
-      <img
-        src={ `//opencollective.com/webpack/${type}/${i}/avatar?requireActive=false` }
-        alt={ `${type} avatar` } />
-    </a>
-  );
-
-  // add missing sponsors
-  if (type === 'sponsor') {
-    MISSING_SPONSORS.forEach(sponsor => {
-      supportItems.splice(sponsor.index, 0, (
-        <a key={ sponsor.name }
-           className="support__item support__missing"
-           href={ sponsor.url }
-           target="_blank"
-           title={ sponsor.title }>
-          <img
-            src={ sponsor.logo }
-            height={ 45 }
-            alt={ sponsor.name } />
-        </a>
-      ));
-    });
+  state = {
+    supporters: [],
+    error: false
   }
 
-  return (
-    <div className="support">
-      {supportItems}
-    </div>
-  );
-};
+  render() {
+    let { number, type } = this.props;
+    let { supporters } = this.state;
+
+    return (
+      <div className="support">
+        {
+          supporters.slice(0, number).map((supporter, i) => (
+            <a key={ supporter.id }
+               className="support__item"
+               target="_blank"
+               href={ supporter.website }>
+              <img
+                className={ `support__${type}-avatar` }
+                src={ supporter.avatar }
+                alt={ `${supporter.username}'s avatar` } />
+              { type === 'backers' ? <figure className="support__outline" /> : null }
+            </a>
+          ))
+        }
+
+        <div className="support__bottom">
+          <a className="support__button" href="https://opencollective.com/webpack#support">
+            Become a { type.replace(/s$/, '') }
+          </a>
+        </div>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    let { type } = this.props;
+
+    fetch(`https://opencollective.com/webpack/${type}.json`)
+      .then(response => response.json())
+      .then(json => this.setState({ supporters: json.concat(Additional) }))
+      .catch(error => this.setState({ error: true }));
+  }
+}
