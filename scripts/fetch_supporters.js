27c4e739c2c3ea73fba8c40c8abeae6d58d98d29
Greg Venech <greg.venech@gmail.com>
2017-05-02 17:53:28 -0400

    refactor: add missing semicolons

--- scripts/fetch_supporters.js
+++ scripts/fetch_supporters.js
@@ -4,18 +4,18 @@ const request = require('request');
 
 request('https://opencollective.com/webpack/sponsors.json', (err, response, body) => {
   if (err) {
-    console.error('Failed to fetch sponsors: ', err)
+    console.error('Failed to fetch sponsors: ', err);
 
   } fs.writeFile('./components/support/support-sponsors.json', body, err => {
-    if (err) console.error('Failed to write sponsors file: ', err)
-  })
-})
+    if (err) console.error('Failed to write sponsors file: ', err);
+  });
+});
 
 request('https://opencollective.com/webpack/backers.json', (err, response, body) => {
   if (err) {
-    console.error('Failed to fetch backers: ', err)
+    console.error('Failed to fetch backers: ', err);
 
   } fs.writeFile('./components/support/support-backers.json', body, err => {
-    if (err) console.error('Failed to write backers file: ', err)
-  })
-})
+    if (err) console.error('Failed to write backers file: ', err);
+  });
+});
