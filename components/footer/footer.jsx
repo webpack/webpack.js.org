cdc02b6839f398af9cd0fcfae286a792b73caf0c
Greg Venech <greg.venech@gmail.com>
2017-05-26 19:34:02 -0400

    refactor(footer): change cc-by image/link and add glossary link

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import CC from '../cc/cc';
 import Link from '../link/link';
 import Container from '../container/container';
 import Icon from '../../assets/icon-square-small.svg';
+import CCBY from '../../assets/cc-by.png';
 import './footer-style';
 
 export default (props) => {
@@ -23,11 +23,15 @@ export default (props) => {
         </section>
 
         <section className="footer__right">
+          <Link className="footer__link" to="/glossary">Glossary</Link>
           <Link className="footer__link" to="/branding">Branding</Link>
           <Link className="footer__link" to="//gitter.im/webpack/webpack">Gitter</Link>
           <Link className="footer__link" to="https://github.com/webpack/webpack/releases">Changelog</Link>
-          <Link className="footer__link" to="/license">License</Link>
-          <CC />
+          <Link className="footer__link footer__license" to="/license">
+            <img
+              alt="Creative Commons License"
+              src={ CCBY } />
+          </Link>
         </section>
       </Container>
     </footer>
