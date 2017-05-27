webpack/webpack.js.org

commit d312c824a3b7d00801b5096a696106ce840497c1

fix(page): resolve `//` issue in edit links

components/page-links/page-links.jsx
@@ -7,7 +7,8 @@ export default ({
   page = {}
 }) => {
   let baseURL = 'https://github.com/webpack/webpack.js.org/edit/master/content';
-  let editLink = page.edit || `${baseURL}/${TrimEnd(page.url, '/')}${page.type === 'index' ? '/index' : ''}.md`;
+  let indexPath = page.type === 'index' ? '/index' : '';
+  let editLink = page.edit || baseURL + TrimEnd(page.url, '/') + indexPath + '.md';
 
   return (
     <div className="page-links">

commit 7318c37c2bf2f19b72d313109600e9e1ea433bbf

refactor(page-links): patch another page link issue caused by d312c824

components/page-links/page-links.jsx
@@ -8,7 +8,8 @@ export default ({
 }) => {
   let baseURL = 'https://github.com/webpack/webpack.js.org/edit/master/content';
   let indexPath = page.type === 'index' ? '/index' : '';
-  let editLink = page.edit || baseURL + TrimEnd(page.url, '/') + indexPath + '.md';
+  let mainPath = page.url.startsWith('/') ? page.url : `/${page.url}`;
+  let editLink = page.edit || baseURL + TrimEnd(mainPath, '/') + indexPath + '.md';
 
   return (
     <div className="page-links">