26c228f97bee1a1834f90919c019f024e88f475b
Juho Vepsalainen <bebraw@gmail.com>
2017-05-27 17:27:49 +0300

    Port page layout to the new system

--- components/page/page.jsx
+++ components/page/page.jsx
@@ -11,62 +11,71 @@ import '../sidebar/sidebar-style';
 import '../sponsors/sponsors-style';
 import '../gitter/gitter-style';
 
-export default ({ section, page }) => (
-  <Container className="page">
+const Page = ({ page, section }) => {
+  const {
+    file: {
+      attributes: {
+        anchors,
+        contributors,
+        title,
+        related,
+      },
+      body
+    },
+    url,
+  } = page;
 
-    <Sponsors />
-
-    <Interactive
-      id="components/sidebar/sidebar.jsx"
-      component={ Sidebar }
-      sectionName={ section.name }
-      pages={ section.pages().map(page => ({
-        url: page.url,
-        title: page.title,
-        anchors: page.anchors
-      })) }
-      currentPage={ page.url.replace('/index', '') } />
-
-    <section className="page__content">
-      <h1>{ page.title }</h1>
-
-      <PageLinks
-        page={ page }
-        section={ section.name } />
-
-      <div dangerouslySetInnerHTML={{
-        __html: page.content
-      }} />
-
-      { page.related.length > 0 ? (
-        <div>
-          <hr />
-          <h3>Further Reading</h3>
-          <ul>
-            {
-              page.related.map((link, index) => (
-                <li key={ index }>
-                  <a href={ link.url }>
-                    { link.title }
-                  </a>
-                </li>
-              ))
-            }
-          </ul>
-        </div>
-      ) : null }
-
-      { page.contributors.length > 0 ? (
-        <div>
-          <hr />
-          <h3>Contributors</h3>
-          <Contributors contributors={ page.contributors } />
-        </div>
-      ) : null }
-
+  return (
+    <Container className="page">
+      <Sponsors />
       <Interactive
-        id="components/gitter/gitter.jsx"
-        component={ Gitter } />
-    </section>
-  </Container>
-);
+        id="components/sidebar/sidebar.jsx"
+        component={ Sidebar }
+        sectionName={ section.name }
+        pages={ section.pages().map(page => ({ url, title, anchors })) }
+        currentPage={ url } />
+
+      <section className="page__content">
+        <h1>{ title }</h1>
+
+        <PageLinks
+          page={ page }
+          section={ section.name } />
+
+        <div dangerouslySetInnerHTML={{ __html: body }} />
+
+        { related.length > 0 && (
+          <div>
+            <hr />
+            <h3>Further Reading</h3>
+            <ul>
+              {
+                related.map((link, index) => (
+                  <li key={ index }>
+                    <a href={ link.url }>
+                      { link.title }
+                    </a>
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
+        )}
+
+        { contributors.length > 0 && (
+          <div>
+            <hr />
+            <h3>Contributors</h3>
+            <Contributors contributors={ contributors } />
+          </div>
+        )}
+
+        <Interactive
+          id="components/gitter/gitter.jsx"
+          component={ Gitter } />
+      </section>
+    </Container>
+  );
+};
+
+export default Page;
