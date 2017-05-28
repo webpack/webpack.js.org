8f0b4f9db3fe719a9d1ecc8c93f9530f6c968323
Greg Venech <greg.venech@gmail.com>
2017-05-28 16:20:34 -0400

    refactor(sidebar): fix introduction link

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -30,7 +30,7 @@ export default class Sidebar extends Component {
           </a>
 
           <SidebarItem
-            url={ `${sectionName}` }
+            url={ `/${sectionName}/` }
             title="Introduction"
             currentPage= { currentPage } />
 
