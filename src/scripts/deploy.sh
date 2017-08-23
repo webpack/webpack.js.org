36503e0a8601872ea02e16d75fd94796a563c4b7
Greg Venech <greg.venech@gmail.com>
2017-08-22 21:54:39 -0400

    fix(deploy): fix deploy script after restructuring
    The location of certain files changed in fd37f672cc.

--- src/scripts/deploy.sh
+++ src/scripts/deploy.sh
@@ -29,10 +29,10 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in scripts/deploy_key.enc -out scripts/deploy_key -d
-chmod 600 scripts/deploy_key
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in src/scripts/deploy_key.enc -out src/scripts/deploy_key -d
+chmod 600 src/scripts/deploy_key
 eval `ssh-agent -s`
-ssh-add scripts/deploy_key
+ssh-add src/scripts/deploy_key
 
 # Now that we're all set up, we can deploy
 npm run deploy
