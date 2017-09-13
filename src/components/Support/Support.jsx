2ff84b97f98c8907e669de36c2936924bc861472
Tobias Koppers <tobias.koppers@googlemail.com>
2017-09-13 14:49:26 +0200

    fix(sponsors): update to reflect opencollective change (#1588)
    OpenCollective's structure changed a bit. This address that
    and makes a few other notable changes:
    
    - add additional sponsors
    - allow to merge additional sponsors to existing sponsors
    - limit width of logos to 3 x height to disallow very wide logos
    - format money values with commas

--- src/components/Support/Support.jsx
+++ src/components/Support/Support.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import Additional from './support-additional.json';
+import GoldSponsors from './support-goldsponsors.json';
+import SilverSponsors from './support-silversponsors.json';
+import Sponsors from './support-sponsors.json';
+import Backers from './support-backers.json';
+import Additional from './support-additional.js';
 import './Support.scss';
 
 const ranks = {
+  backer: {
+    maximum: 200
+  },
   bronze: {
+    minimum: 200,
     maximum: 2000
   },
   silver: {
@@ -19,16 +27,36 @@ const ranks = {
   }
 };
 
+function formatMoney(number) {
+  let str = Math.round(number) + '';
+  if (str.length > 3) {
+    str = str.substr(0, str.length - 3) + ',' + str.substr(-3);
+  }
+  return str;
+}
+
 export default class Support extends React.Component {
   render() {
-    let { rank, type } = this.props;
-    let supporters = require(`./support-${type}.json`);
+    let { rank } = this.props;
+    let supporters = [
+      ...GoldSponsors,
+      ...SilverSponsors,
+      ...Sponsors,
+      ...Backers,
+    ];
 
-    if (type === 'sponsors') {
-      supporters = supporters.slice();
-      supporters.push(...Additional);
-      supporters.sort((a, b) => b.totalDonations - a.totalDonations);
+    // merge or add additional backers/sponsors
+    for(const additional of Additional) {
+      const existing = supporters.find(supporter => supporter.username && supporter.username === additional.username);
+      if (existing) {
+        existing.totalDonations += additional.totalDonations;
+      } else {
+        supporters.push(additional);
+      }
     }
+
+    // resort list
+    supporters.sort((a, b) => b.totalDonations - a.totalDonations);
 
     let minimum, maximum;
 
@@ -48,14 +76,14 @@ export default class Support extends React.Component {
     return (
       <div className="support">
         <div className="support__description">
-          { type === 'sponsors' ? (
+          { rank === 'backer' ? (
             <p>
-              <b className="support__rank">{ rank } sponsors</b>
-              <span>are those who have pledged { minimum ? `$${minimum}` : 'up' } { maximum ? `to $${maximum}` : 'or more' } to webpack.</span>
+              The following <b>Backers</b> are individuals who have contributed various amounts of money in order to help support webpack. Every little bit helps, and we appreciate even the smallest contributions.
             </p>
           ) : (
             <p>
-              The following <b>Backers</b> are individuals who have contributed various amounts of money in order to help support webpack. Every little bit helps, and we appreciate even the smallest contributions.
+              <b className="support__rank">{ rank } sponsors</b>
+              <span>are those who have pledged { minimum ? `$${formatMoney(minimum)}` : 'up' } { maximum ? `to $${formatMoney(maximum)}` : 'or more' } to webpack.</span>
             </p>
           )}
         </div>
@@ -64,22 +92,22 @@ export default class Support extends React.Component {
           supporters.map((supporter, index) => (
             <a key={ supporter.id || supporter.username || index }
                className="support__item"
-               title={ `$${supporter.totalDonations / 100} by ${supporter.name || supporter.username}` }
+               title={ `$${formatMoney(supporter.totalDonations / 100)} by ${supporter.name || supporter.username}` }
                target="_blank"
                href={ supporter.website || `https://opencollective.com/${supporter.username}` }>
               { supporter.avatar ? <img
-                className={ `support__${type}-avatar-${rank || 'normal'}` }
+                className={ `support__${rank}-avatar` }
                 src={ supporter.avatar }
                 alt={ supporter.username ? `${supporter.username}'s avatar` : 'avatar' } /> :
                 supporter.name }
-              { type === 'backers' ? <figure className="support__outline" /> : null }
+              { rank === 'backer' ? <figure className="support__outline" /> : null }
             </a>
           ))
         }
 
         <div className="support__bottom">
           <a className="support__button" href="https://opencollective.com/webpack#support">
-            Become a { type.replace(/s$/, '') }
+            Become a { rank === 'backer' ? 'backer' : 'sponsor' }
           </a>
         </div>
       </div>
