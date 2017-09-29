77259b42ab051cabedb5c9ab2c1313dc8cafc8b4
Greg Venech <greg.venech@gmail.com>
2017-09-29 00:09:41 -0400

    refactor: update information architecture
    This commit contains the following changes:
    
    - Merge support and development into a coherent "Contribute" section.
    - Flatten `/api/plugins` in preparation for flattened page `group`ing.
    - Sort "Contribute" section and rewrite index page using "Why Support?".
    - Simplify and organize antwar config as much as possible.
    - Move `title` / `description` fields to the actual components.
    
    BREAKING CHANGE: The route restructuring and content reorg
    breaks some routes. Before this is merged, we should make sure
    to add redirects for anything broken.

--- src/components/Splash/Splash.jsx
+++ src/components/Splash/Splash.jsx
@@ -47,4 +47,7 @@ const Splash = () => (
   </div>
 );
 
+Splash.title = 'webpack';
+Splash.description = 'webpack is a module bundler. Its main purpose is to bundle JavaScript files for usage in a browser, yet it is also capable of transforming, bundling, or packaging just about any resource or asset.';
+
 export default Splash;
--- src/content/api/parser.md
+++ src/content/api/parser.md
@@ -1,5 +1,6 @@
 ---
 title: Parser
+group: Plugins
 sort: 8
 ---
 
