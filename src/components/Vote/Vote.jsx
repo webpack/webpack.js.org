73d3f1a2f71d3c50d94021a21c71e5f621bc0dae
Greg Venech <greg.venech@gmail.com>
2017-11-26 16:07:44 -0500

    style(vote): add some very simple header styling for consistency
    We may be able to figure out a cleaner way to do this but for now this keeps
    things pretty consistent with the rest of the site. In general the voting app should
    probably do most of it's own styling though -- however we should probably
    include the same `reset` stylesheet in both places for consistency.

--- src/components/Vote/Vote.jsx
+++ src/components/Vote/Vote.jsx
@@ -3,6 +3,7 @@ import Interactive from 'antwar-interactive';
 import Container from '../Container/Container';
 import VoteApp from 'webpack.vote/dist/vote.bundle.js';
 import 'webpack.vote/dist/style.min.css';
+import './Vote.scss';
 
 const Vote = ({ section, page }) => (
   <Container className="vote">
