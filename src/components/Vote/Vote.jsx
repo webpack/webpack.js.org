74e003866fab58116dd227233970e98f9e1fd0d4
Greg Venech <greg.venech@gmail.com>
2017-12-21 01:47:39 -0500

    refactor(vote): fix issue with antwar-interactive's `id` resolving

--- src/components/Vote/Vote.jsx
+++ src/components/Vote/Vote.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Interactive from 'antwar-interactive';
 import Container from '../Container/Container';
-import VoteApp from 'webpack.vote/dist/vote.bundle.js';
+import VoteApp from 'webpack.vote';
 import 'webpack.vote/dist/style.min.css';
 import './Vote.scss';
 
 const Vote = ({ section, page }) => (
   <Container className="vote">
     <Interactive
-      id="webpack.vote/dist/vote.bundle.js"
+      id="./node_modules/webpack.vote"
       component={ VoteApp }
       // development={ true }
     />
