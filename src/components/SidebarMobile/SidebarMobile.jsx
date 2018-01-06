b1b8dcaf9b4165375d319e425794ca81fa0cdde9
Greg Venech <greg.venech@gmail.com>
2018-01-06 13:51:56 -0500

    refactor(sidebar-mobile): use class properties, stateless methods, and respond to `open` prop
    More refactoring to come (as hinted by the `TODO`s)...

--- src/components/SidebarMobile/SidebarMobile.jsx
+++ src/components/SidebarMobile/SidebarMobile.jsx
@@ -2,35 +2,34 @@ import React from 'react';
 import Link from '../Link/Link';
 import './SidebarMobile.scss';
 
-let initialTouchPosition = {};
-let lastTouchPosition = {};
-
+// TODO: Finish updating close and swipe behaviors
 export default class SidebarMobile extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this._handleBodyClick = this._handleBodyClick.bind(this);
-  }
+  _container = null
+  _initialTouchPosition = {}
+  _lastTouchPosition = {}
 
   render() {
+    let { open } = this.props;
+    let openMod = open ? 'sidebar-mobile--visible' : '';
+
     return (
       <nav
-        className="sidebar-mobile"
-        ref={ ref => this.container = ref }
-        onTouchStart={this._handleTouchStart.bind(this)}
-        onTouchMove={this._handleTouchMove.bind(this)}
-        onTouchEnd={this._handleTouchEnd.bind(this)}>
+        className={ `sidebar-mobile ${openMod}` }
+        ref={ ref => this._container = ref }
+        onTouchStart={ this._handleTouchStart }
+        onTouchMove={ this._handleTouchMove }
+        onTouchEnd={ this._handleTouchEnd }>
 
         <div
           className="sidebar-mobile__toggle"
-          onTouchStart={this._handleTouchStart.bind(this)}
-          onTouchMove={this._handleOpenerTouchMove.bind(this)}
-          onTouchEnd={this._handleTouchEnd.bind(this)} />
+          onTouchStart={ this._handleTouchStart }
+          onTouchMove={ this._handleOpenerTouchMove }
+          onTouchEnd={ this._handleTouchEnd } />
 
         <div className="sidebar-mobile__content">
           <i
             className="sidebar-mobile__close icon-cross"
-            onClick={ this._close.bind(this) } />
+            onClick={ this.props.toggle } />
 
           { this._getSections() }
         </div>
@@ -40,14 +39,14 @@ export default class SidebarMobile extends React.Component {
 
   componentDidMount() {
     if (typeof window !== 'undefined') {
-      window.addEventListener('click', this._handleBodyClick);
+      // window.addEventListener('click', this._handleBodyClick);
       window.addEventListener('touchstart', this._handleBodyClick);
     }
   }
 
   componentWillUnmount() {
     if (typeof window !== 'undefined') {
-      window.removeEventListener('click', this._handleBodyClick);
+      // window.removeEventListener('click', this._handleBodyClick);
       window.removeEventListener('touchstart', this._handleBodyClick);
     }
   }
@@ -120,75 +119,75 @@ export default class SidebarMobile extends React.Component {
    *
    * @param {object} e - Native click event
    */
-  _handleBodyClick = e => {
-    if (
-      !e.target.classList.contains('icon-menu') &&
-      !this.container.contains(e.target)
-    ) {
-      this._close();
-    }
-  }
+  // _handleBodyClick = e => {
+  //   if (
+  //     this.props.open &&
+  //     !this._container.contains(e.target)
+  //   ) {
+  //     this._close();
+  //   }
+  // }
 
   /**
    * Hide the sidebar
    *
    */
-  _close() {
-    this.container.classList.remove(
-      'sidebar-mobile--visible'
-    );
-  }
+  // _close() {
+  //   this._container.classList.remove(
+  //     'sidebar-mobile--visible'
+  //   );
+  // }
 
-  _open() {
-    this.container.classList.add(
-      'sidebar-mobile--visible'
-    );
-  }
+  // _open() {
+  //   this._container.classList.add(
+  //     'sidebar-mobile--visible'
+  //   );
+  // }
 
-  _handleTouchStart(e){
-    initialTouchPosition.x = e.touches[0].pageX;
-    initialTouchPosition.y = e.touches[0].pageY;
+  _handleTouchStart = e => {
+    this._initialTouchPosition.x = e.touches[0].pageX;
+    this._initialTouchPosition.y = e.touches[0].pageY;
 
     // For instant transform along with the touch
-    this.container.classList.add('no-delay');
+    this._container.classList.add('no-delay');
   }
 
-  _handleTouchMove(e){
-    let xDiff = initialTouchPosition.x - e.touches[0].pageX;
-    let yDiff = initialTouchPosition.y - e.touches[0].pageY;
+  _handleTouchMove = e => {
+    let xDiff = this._initialTouchPosition.x - e.touches[0].pageX;
+    let yDiff = this._initialTouchPosition.y - e.touches[0].pageY;
     let factor = Math.abs(yDiff / xDiff);
 
     // Factor makes sure horizontal and vertical scroll dont take place together
     if (xDiff>0 && factor < 0.8) {
       e.preventDefault();
-      this.container.style.transform = `translateX(-${xDiff}px)`;
-      lastTouchPosition.x = e.touches[0].pageX;
-      lastTouchPosition.y = e.touches[0].pageY;
+      this._container.style.transform = `translateX(-${xDiff}px)`;
+      this._lastTouchPosition.x = e.touches[0].pageX;
+      this._lastTouchPosition.y = e.touches[0].pageY;
     }
   }
 
-  _handleOpenerTouchMove(e){
-    let xDiff = e.touches[0].pageX - initialTouchPosition.x;
-    let yDiff = initialTouchPosition.y - e.touches[0].pageY;
+  _handleOpenerTouchMove = e => {
+    let xDiff = e.touches[0].pageX - this._initialTouchPosition.x;
+    let yDiff = this._initialTouchPosition.y - e.touches[0].pageY;
     let factor = Math.abs(yDiff / xDiff);
 
     // Factor makes sure horizontal and vertical scroll dont take place together
     if (xDiff > 0 && xDiff < 295 && factor < 0.8) {
       e.preventDefault();
-      this.container.style.transform = `translateX(calc(-100% + ${xDiff}px))`;
-      lastTouchPosition.x = e.touches[0].pageX;
-      lastTouchPosition.y = e.touches[0].pageY;
+      this._container.style.transform = `translateX(calc(-100% + ${xDiff}px))`;
+      this._lastTouchPosition.x = e.touches[0].pageX;
+      this._lastTouchPosition.y = e.touches[0].pageY;
     }
   }
 
-  _handleTouchEnd(e){
+  _handleTouchEnd = e => {
     // Free up all the inline styling
-    this.container.classList.remove('no-delay');
-    this.container.style.transform = '';
+    this._container.classList.remove('no-delay');
+    this._container.style.transform = '';
 
-    if (initialTouchPosition.x - lastTouchPosition.x > 100) {
+    if (this._initialTouchPosition.x - this._lastTouchPosition.x > 100) {
       this._close();
-    } else if (lastTouchPosition.x - initialTouchPosition.x > 100) {
+    } else if (this._lastTouchPosition.x - this._initialTouchPosition.x > 100) {
       this._open();
     }
   }
