0957e3f4c3b2c515d6003f4005ec393b2f995340
Greg Venech <greg.venech@gmail.com>
2018-01-06 03:53:44 -0500

    refactor(navigation): utilize `react-banner` remove hardcoded links
    The 	`react-banner` package was built based off of this component
    but has a few fixes and will externalize any bugs so this site can
    focus more on content. It also allows anyone else to utilize the banner
    in other sites.
    
    Still a few `TODO`s left...

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,173 +1,67 @@
 import React from 'react';
+import Banner from 'react-banner';
 import Link from '../Link/Link';
 import Container from '../Container/Container';
 import Logo from '../Logo/Logo';
 import Dropdown from '../Dropdown/Dropdown';
-import Links from './Links.json';
+import './Navigation.scss';
+import './Search.scss';
 
-// TODO: Migrate to React Banner
+// TODO: Re-incorporate the icon links
+//  <Link
+//    className="navigation__icon"
+//    title="GitHub Repository"
+//    to="//github.com/webpack/webpack">
+//    <i className="sidecar__icon icon-github" />
+//  </Link>
+//  <Link
+//    className="navigation__icon"
+//    title="See Questions on Stack Overflow"
+//    to="//stackoverflow.com/questions/tagged/webpack">
+//    <i className="sidecar__icon icon-stack-overflow" />
+//  </Link>
+//  <Dropdown
+//    className="navigation__languages"
+//    items={[
+//      { title: 'English', url: 'https://webpack.js.org/' },
+//      { title: '中文', url: 'https://doc.webpack-china.org/' }
+//    ]} />
+
+// TODO: Re-incorporate docsearch (see `react-banner` docs and `SearchResults` component/discussion)
+//   componentDidMount() {
+//     if (typeof window !== 'undefined') {
+//       let docsearch = () => {};
+
+//       // XXX: hack around docsearch
+//       if (window.docsearch) {
+//         docsearch = window.docsearch.default || window.docsearch;
+//       }
+
+//       docsearch({
+//         apiKey: 'fac401d1a5f68bc41f01fb6261661490',
+//         indexName: 'webpack-js-org',
+//         inputSelector: '.navigation__search-input'
+//       });
+
+//       window.addEventListener('keyup', e => {
+//         if (e.which === 9 && e.target.classList.contains('navigation__search-input')) {
+//           this._openSearch();
+//         }
+//       });
+//     }
+//   }
+
 export default class Navigation extends React.Component {
   render() {
-    let { pageUrl = '' } = this.props;
+    let { pathname, links, toggleSidebar } = this.props;
 
     return (
-      <header className="navigation">
-        <Container className="navigation__inner">
-          <div className="navigation__mobile" onClick={ this._toggleSidebar }>
-            <i className="icon-menu" />
-          </div>
-
-          <Link className="navigation__logo" to="/">
-            <Logo light={ true } />
-          </Link>
-
-          <nav className="navigation__links">
-            { Links.map(link => {
-              let active = this._isActive(link);
-              let activeMod = active ? 'navigation__link--active' : '';
-
-              return (
-                <Link
-                  key={ `navigation__link-${link.title}` }
-                  className={ `navigation__link ${activeMod}` }
-                  to={ `/${link.url}/` }>
-                  { link.title }
-                </Link>
-              );
-            }) }
-          </nav>
-
-          <div className="navigation__search">
-            <input
-              type="text"
-              className="navigation__search-input"
-              placeholder="Search documentation…"
-              onBlur={ this._toggleSearch.bind(this) } />
-            <button
-              className="navigation__search-icon icon-magnifying-glass"
-              onClick={ this._toggleSearch.bind(this) } />
-            <button
-              className="navigation__search-icon icon-cross"
-              onClick={ this._toggleSearch.bind(this) } />
-          </div>
-
-          <Link
-            className="navigation__icon"
-            title="GitHub Repository"
-            to="//github.com/webpack/webpack">
-            <i className="sidecar__icon icon-github" />
-          </Link>
-
-          <Link
-            className="navigation__icon"
-            title="See Questions on Stack Overflow"
-            to="//stackoverflow.com/questions/tagged/webpack">
-            <i className="sidecar__icon icon-stack-overflow" />
-          </Link>
-
-          <Dropdown
-            className="navigation__languages"
-            items={[
-              { title: 'English', url: 'https://webpack.js.org/' },
-              { title: '中文', url: 'https://doc.webpack-china.org/' }
-            ]} />
-        </Container>
-
-        { Links.filter(link => this._isActive(link) && link.children).map(link => (
-          <div className="navigation__bottom" key={ link.title }>
-            <Container className="navigation__inner">
-              {
-                link.children.map(child => {
-                  let activeMod = this._isActive(child) ? 'navigation__child--active' : '';
-
-                  return (
-                    <Link
-                      key={ `navigation__child-${child.title}` }
-                      className={ `navigation__child ${activeMod}` }
-                      to={ `/${child.url}/` }>
-                      { child.title }
-                    </Link>
-                  );
-                })
-              }
-            </Container>
-          </div>
-        )) }
-      </header>
+      <Banner
+        logo={ <Logo light={ true } /> }
+        url={ pathname }
+        links={ links }
+        link={ Link }
+        onMenuClick={ toggleSidebar } />
     );
-  }
-
-  componentDidMount() {
-    if (typeof window !== 'undefined') {
-      let docsearch = () => {};
-
-      // XXX: hack around docsearch
-      if (window.docsearch) {
-        docsearch = window.docsearch.default || window.docsearch;
-      }
-
-      docsearch({
-        apiKey: 'fac401d1a5f68bc41f01fb6261661490',
-        indexName: 'webpack-js-org',
-        inputSelector: '.navigation__search-input'
-      });
-
-      window.addEventListener('keyup', e => {
-        if (e.which === 9 && e.target.classList.contains('navigation__search-input')) {
-          this._openSearch();
-        }
-      });
-    }
-  }
-
-  /**
-   * Check if the given `link` is active
-   *
-   * @param  {object} link - An object describing the `link`
-   * @return {bool}        - Whether or not the given `link` is active
-   */
-  _isActive(link) {
-    let { pageUrl = '' } = this.props;
-
-    if (link.children) {
-      return link.children.some(child => {
-        return (new RegExp("^/" + child.url + ".*/")).test(pageUrl);
-      });
-
-    } else return (new RegExp("^/" + link.url +".*/")).test(pageUrl);
-  }
-
-  /**
-   * Toggle the SidebarMobile component
-   *
-   * @param {object} e - Native click event
-   */
-  _toggleSidebar(e) {
-    let sidebar = document.querySelector('.sidebar-mobile');
-    let modifier = 'sidebar-mobile--visible';
-
-    sidebar.classList.toggle(modifier);
-  }
-
-  /**
-   * Toggle the search input
-   *
-   */
-  _toggleSearch() {
-    let container = document.querySelector('.navigation');
-    let input = document.querySelector('.navigation__search-input');
-    let state = container.classList.toggle('navigation--search-mode');
-
-    if ( state === true ) input.focus();
-  }
-
-  /**
-   * Expand the search input
-   *
-   */
-  _openSearch() {
-    let container = document.querySelector('.navigation');
-
-    container.classList.add('navigation--search-mode');
   }
 }
