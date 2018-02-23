079de38b3d8f7edabe69a7cb8cb7def335ddd899
Gabriel Ricard <4550801+gricard@users.noreply.github.com>
2018-02-23 17:52:17 -0500

    Added notification for webpack 4 beta availability w/links to articles

--- src/components/NotificationBar/NotificationBar.jsx
+++ src/components/NotificationBar/NotificationBar.jsx
@@ -13,6 +13,12 @@ export default class NotificationBar extends React.Component {
       <div className={ `notification-bar ${dismissedMod}` }>
         <Container className="notification-bar__inner">
           <p>
+            WEBPACK 4 IS IN BETA, TRY IT TODAY, SEE THESE ARTICLES: <br />
+            <a target="_blank" href="https://medium.com/webpack/webpack-4-beta-try-it-today-6b1d27d7d7e2">* Install and setup Webpack 4</a><br />
+            <a target="_blank" href="https://medium.com/webpack/webpack-4-mode-and-optimization-5423a6bc597a">* New configuration options in Webpack 4</a><br />
+          </p>
+          <br />
+          <p>
             Sponsor webpack and get apparel from the <a href="https://webpack.threadless.com">official shop</a>{' '}
             or get stickers <a href="http://www.unixstickers.com/tag/webpack">here</a>! All proceeds go to our{' '}
             <a href="https://opencollective.com/webpack">open collective</a>!
