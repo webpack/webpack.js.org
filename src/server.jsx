19d8e3944822317d491b8827a91ae5ce8efcea50
Greg Venech <greg.venech@gmail.com>
2018-03-24 14:37:45 -0400

    chore: add main `description` to site

--- src/server.jsx
+++ src/server.jsx
@@ -32,7 +32,7 @@ export default locals => {
           <meta name="theme-color" content="#2B3A42" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <title>{/* TODO */} | webpack</title>
-          <meta name="description" content={''/* TODO */} />
+          <meta name="description" content="webpack is a module bundler. Its main purpose is to bundle JavaScript files for usage in a browser, yet it is also capable of transforming, bundling, or packaging just about any resource or asset." />
           <link rel="icon" type="image/x-icon" href={ Favicon } />
           { Object.keys(assets).filter(asset => /\.css$/.test(asset)).map(path => (
             <link key={ path } rel="stylesheet" href={ `/${path}` } />
