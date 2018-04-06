d4e02ec343bac3776af3291fad33e7fc73b497d1
Travis CI <ci@travis-ci.org>
2018-04-06 19:39:34 +0200

    chore(site) Uncomment Travis key management

--- src/scripts/deploy.sh
+++ src/scripts/deploy.sh
@@ -27,14 +27,14 @@ git config --global user.email "ci@travis-ci.org"
 git remote set-url origin "${SSH_REPO}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-# ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-# ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-# ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-# ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-# openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in src/scripts/deploy_key.enc -out src/scripts/deploy_key -d
-# chmod 600 src/scripts/deploy_key
-# eval `ssh-agent -s`
-# ssh-add src/scripts/deploy_key
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in src/scripts/deploy_key.enc -out src/scripts/deploy_key -d
+chmod 600 src/scripts/deploy_key
+eval `ssh-agent -s`
+ssh-add src/scripts/deploy_key
 
 git checkout fix/v3-docs
 npm run build
