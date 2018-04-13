247b4ced082fca4a9d6c6f84cbee106c0c26f50c
Fernando Montoya <montogeek@gmail.com>
2018-04-13 11:39:01 +0200

    fix(site) Partially revert #2026 (#2033)

--- antwar.config.js
+++ antwar.config.js
@@ -14,69 +14,69 @@ module.exports = () => ({
     '/': {
       title: 'Home',
       layout: () => require('./src/components/Page/Page.jsx').default,
-      content: () => require.context('./src/content', false, /^\.\/.*\.md$/),
-      index: () => require('./src/components/Splash/Splash.jsx').default,
+      content: () => require.context('./loaders/page-loader!./src/content', false, /^\.\/.*\.md$/),
+      index: () => require('./src/components/Splash/Splash.jsx').default
     },
     concepts: {
       title: 'Concepts',
-      url: ({sectionName, fileName}) => `/${sectionName}/${fileName}/`,
+      url: ({ sectionName, fileName }) => `/${sectionName}/${fileName}/`,
       layout: () => require('./src/components/Page/Page.jsx').default,
-      content: () => require.context('./src/content/concepts', false, /^\.\/.*\.md$/)
+      content: () => require.context('./loaders/page-loader!./src/content/concepts', false, /^\.\/.*\.md$/)
     },
     configuration: {
       title: 'Configuration',
-      url: ({sectionName, fileName}) => `/${sectionName}/${fileName}/`,
+      url: ({ sectionName, fileName }) => `/${sectionName}/${fileName}/`,
       layout: () => require('./src/components/Page/Page.jsx').default,
-      content: () => require.context('./src/content/configuration', false, /^\.\/.*\.md$/)
+      content: () => require.context('./loaders/page-loader!./src/content/configuration', false, /^\.\/.*\.md$/)
     },
     api: {
       title: 'API',
-      url: ({sectionName, fileName}) => `/${sectionName}/${fileName}/`,
+      url: ({ sectionName, fileName }) => `/${sectionName}/${fileName}/`,
       layout: () => require('./src/components/Page/Page.jsx').default,
-      content: () => require.context('./src/content/api', false, /^\.\/.*\.md$/)
+      content: () => require.context('./loaders/page-loader!./src/content/api', false, /^\.\/.*\.md$/)
     },
     guides: {
       title: 'Guides',
-      url: ({sectionName, fileName}) => `/${sectionName}/${fileName}/`,
+      url: ({ sectionName, fileName }) => `/${sectionName}/${fileName}/`,
       layout: () => require('./src/components/Page/Page.jsx').default,
-      content: () => require.context('./src/content/guides', false, /^\.\/.*\.md$/)
+      content: () => require.context('./loaders/page-loader!./src/content/guides', false, /^\.\/.*\.md$/)
     },
     plugins: {
       title: 'Plugins',
-      url: ({sectionName, fileName}) => `/${sectionName}/${fileName}/`,
+      url: ({ sectionName, fileName }) => `/${sectionName}/${fileName}/`,
       layout: () => require('./src/components/Page/Page.jsx').default,
       content: () => {
         return combineContexts(
-          require.context('./src/content/plugins', false, /^\.\/.*\.md$/),
-          require.context('./generated/plugins', false, /^\.\/.*\.md$/)
+          require.context('./loaders/page-loader!./src/content/plugins', false, /^\.\/.*\.md$/),
+          require.context('./loaders/page-loader!./generated/plugins', false, /^\.\/.*\.md$/)
         );
       }
     },
     loaders: {
       title: 'Loaders',
-      url: ({sectionName, fileName}) => `/${sectionName}/${fileName}/`,
+      url: ({ sectionName, fileName }) => `/${sectionName}/${fileName}/`,
       layout: () => require('./src/components/Page/Page.jsx').default,
       content: () => {
         return combineContexts(
-          require.context('./src/content/loaders', false, /^\.\/.*\.md$/),
-          require.context('./generated/loaders', false, /^\.\/.*\.md$/)
+          require.context('./loaders/page-loader!./src/content/loaders', false, /^\.\/.*\.md$/),
+          require.context('./loaders/page-loader!./generated/loaders', false, /^\.\/.*\.md$/)
         );
       }
     },
     contribute: {
       title: 'Contribute',
-      url: ({sectionName, fileName}) => `/${sectionName}/${fileName}/`,
+      url: ({ sectionName, fileName }) => `/${sectionName}/${fileName}/`,
       layout: () => require('./src/components/Page/Page.jsx').default,
-      content: () => require.context('./src/content/contribute', false, /^\.\/.*\.md$/),
+      content: () => require.context('./loaders/page-loader!./src/content/contribute', false, /^\.\/.*\.md$/)
     },
     migrate: {
       title: 'Migrate',
       url: ({ sectionName, fileName }) => `/${sectionName}/${fileName}/`,
       layout: () => require('./src/components/Page/Page.jsx').default,
-      content: () => require.context('./src/content/migrate', false, /^\.\/.*\.md$/),
+      content: () => require.context('./loaders/page-loader!./src/content/migrate', false, /^\.\/.*\.md$/)
     },
     vote: () => require('./src/components/Vote/Vote.jsx').default,
     organization: () => require('./src/components/Organization/Organization.jsx').default,
-    'starter-kits': () => require('./src/components/StarterKits/StarterKits.jsx').default,
+    'starter-kits': () => require('./src/components/StarterKits/StarterKits.jsx').default
   }
 });
