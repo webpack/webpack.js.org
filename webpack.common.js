3802721ed2b90a3a6d6e924131a40f0ac40c312a
EugeneHlushko <jhlushko@gmail.com>
2018-04-14 18:13:51 +0300

    feature(redirects) Re-incorporate redirects

--- webpack.common.js
+++ webpack.common.js
@@ -6,6 +6,7 @@ const CleanPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const DirectoryTreePlugin = require('directory-tree-webpack-plugin');
+const RedirectWebpackPlugin = require('redirect-webpack-plugin');
 
 module.exports = (env = {}) => ({
   devtool: 'source-map',
@@ -127,6 +128,39 @@ module.exports = (env = {}) => ({
           // TODO: Add directory (section) attributes and index url (if necessary)
         }
       }
+    }),
+    new RedirectWebpackPlugin({
+      redirects: {
+        'support': '/contribute/',
+        'writers-guide': '/contribute/writers-guide/',
+        'get-started': '/guides/getting-started/',
+        'get-started/install-webpack': '/guides/installation/',
+        'get-started/why-webpack': '/guides/why-webpack/',
+        'pluginsapi': '/api/plugins/',
+        'pluginsapi/compiler': '/api/compiler-hooks/',
+        'pluginsapi/template': '/api/template/',
+        'api/passing-a-config': '/configuration/configuration-types/',
+        'api/plugins/compiler': '/api/compiler-hooks/',
+        'api/plugins/compilation': '/api/compilation/',
+        'api/plugins/module-factories': '/api/module-methods/',
+        'api/plugins/parser': '/api/parser/',
+        'api/plugins/tapable': '/api/tapable/',
+        'api/plugins/template': '/api/template/',
+        'api/plugins/resolver': '/api/resolver/',
+        'development': '/contribute/',
+        'development/plugin-patterns': '/contribute/plugin-patterns/',
+        'development/release-process': '/contribute/release-process/',
+        'development/how-to-write-a-loader': '/contribute/writing-a-loader/',
+        'development/how-to-write-a-plugin': '/contribute/writing-a-plugin/',
+        'guides/code-splitting-import': '/guides/code-splitting/',
+        'guides/code-splitting-require': '/guides/code-splitting/',
+        'guides/code-splitting-async': '/guides/code-splitting/',
+        'guides/code-splitting-css': '/guides/code-splitting/',
+        'guides/code-splitting-libraries': '/guides/code-splitting/',
+        'guides/why-webpack': '/comparison/',
+        'guides/production-build': '/guides/production/',
+        'migrating': '/migrate/3/'
+      },
     })
   ],
   stats: {
