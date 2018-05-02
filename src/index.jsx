7ba006c60012adeb46c969ba1a249bc536b527a1
Greg Venech <greg.venech@gmail.com>
2018-05-02 00:18:02 -0400

    chore(rebuild): preload entry page to avoid flash during retrieval
    This change delays the dynamic part of app from mounting until the first page's
    content has loaded. This prevents the static content from being stripped from the
    DOM until dynamic bundle is there to replace it. Once we migrate to `remark-react`,
    the elements won't even be re-mounted as React's diffing algorithm will be used.
    
    All other pages are still loaded using dynamic `import()`s.

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,16 @@
+// Import External Dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
+
+// Import Components
 import Site from './components/Site/Site';
+
+// Import Utilities
+import { FindInContent } from './utilities/content-utils';
+
+// Import Content Tree
+import Content from './_content.json';
 
 // TODO: Re-integrate <GoogleAnalytics analyticsId="UA-46921629-2" />
 // Consider `react-g-analytics` package
@@ -10,15 +19,27 @@ const render = process.NODE_ENV === 'production' ? ReactDOM.hydrate : ReactDOM.r
 
 // Client Side Rendering
 if ( window.document !== undefined ) {
-  render((
-    <BrowserRouter>
-      <Route
-        path="/"
-        render={ props => (
-          <Site
-            { ...props }
-            import={ path => import(`./content/${path}`) } />
-        )} />
-    </BrowserRouter>
-  ), document.getElementById('root'));
+  let { pathname } = window.location;
+  let trimmed = pathname.replace(/(.+)\/$/, '$1');
+  let entryPage = FindInContent(Content, item => item.url === trimmed);
+  let entryPath = entryPage.path.replace('src/content/', '');
+
+  import(`./content/${entryPath}`).then(entryModule => {
+    render((
+      <BrowserRouter>
+        <Route
+          path="/"
+          render={ props => (
+            <Site
+              { ...props }
+              import={ path => {
+                if ( path === entryPath ) {
+                  return entryModule.default || entryModule;
+
+                } else return import(`./content/${path}`);
+              }} />
+          )} />
+      </BrowserRouter>
+    ), document.getElementById('root'));
+  });
 }
