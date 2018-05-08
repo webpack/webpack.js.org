3bfdf66b2ba1821829e70209676d5d815f71ef71
Jere Menichelli <jmenichelli@gmail.com>
2018-05-09 00:14:38 +0200

    feat: move Placeholder to a component folder

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -6,30 +6,10 @@ import PageLinks from '../PageLinks/PageLinks';
 import Markdown from '../Markdown/Markdown';
 import Contributors from '../Contributors/Contributors';
 import Gitter from '../Gitter/Gitter';
+import Placeholder from '../Placeholder/Placeholder';
 
 // Load Styling
 import './Page.scss';
-
-// Placeholder string
-const Placeholder = () => (`
-  <div class="placeholder">
-    <p class="placeholder--medium">&nbsp;</p>
-    <p class="placeholder--large">&nbsp;</p>
-    <p class="placeholder--small">&nbsp;</p>
-    <h2 class="placeholder--xsmall">&nbsp;</h2>
-    <p class="placeholder--large">&nbsp;</p>
-    <p class="placeholder--small">&nbsp;</p>
-    <p class="placeholder--medium">&nbsp;</p>
-    <h2 class="placeholder--small">&nbsp;</h2>
-    <p class="placeholder--large">&nbsp;</p>
-    <p class="placeholder--medium">&nbsp;</p>
-    <p class="placeholder--xsmall">&nbsp;</p>
-    <h2 class="placeholder--xsmall">&nbsp;</h2>
-    <p class="placeholder--large">&nbsp;</p>
-    <p class="placeholder--small">&nbsp;</p>
-    <p class="placeholder--medium">&nbsp;</p>
-  </div>
-`);
 
 class Page extends React.Component {
   constructor(props) {
---
+++ src/components/Placeholder/Placeholder.js
@@ -0,0 +1,25 @@
+// Import Styling
+import './Placeholder.scss';
+
+// Placeholder string
+const Placeholder = () => (`
+  <div class="placeholder">
+    <p class="placeholder--medium">&nbsp;</p>
+    <p class="placeholder--large">&nbsp;</p>
+    <p class="placeholder--small">&nbsp;</p>
+    <h2 class="placeholder--xsmall">&nbsp;</h2>
+    <p class="placeholder--large">&nbsp;</p>
+    <p class="placeholder--small">&nbsp;</p>
+    <p class="placeholder--medium">&nbsp;</p>
+    <h2 class="placeholder--small">&nbsp;</h2>
+    <p class="placeholder--large">&nbsp;</p>
+    <p class="placeholder--medium">&nbsp;</p>
+    <p class="placeholder--xsmall">&nbsp;</p>
+    <h2 class="placeholder--xsmall">&nbsp;</h2>
+    <p class="placeholder--large">&nbsp;</p>
+    <p class="placeholder--small">&nbsp;</p>
+    <p class="placeholder--medium">&nbsp;</p>
+  </div>
+`);
+
+export default Placeholder;
