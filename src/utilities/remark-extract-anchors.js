c7babca41d70a1134baa5f18485af5c70ec161bd
Jere Menichelli <jmenichelli@gmail.com>
2018-05-11 23:10:24 +0200

    chore: remove logs

--- src/utilities/remark-extract-anchors.js
+++ src/utilities/remark-extract-anchors.js
@@ -19,7 +19,6 @@ module.exports = function extractAnchors(options = {}) {
 
   function visitor(node) {
     // TODO: Default header `levels` and check it to filter the `push`ed anchors
-    console.log(node, node.children)
     options.anchors.push({
       title: node.children.length ? node.children[0].value : '',
       id: node.data.id
