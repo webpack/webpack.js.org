26b1bf1291580c867232a5795709b95b4d1c36b7
Jere Menichelli <jmenichelli@gmail.com>
2018-05-11 23:05:35 +0200

    fix: empty headings fix on anchors

--- src/utilities/remark-extract-anchors.js
+++ src/utilities/remark-extract-anchors.js
@@ -19,8 +19,9 @@ module.exports = function extractAnchors(options = {}) {
 
   function visitor(node) {
     // TODO: Default header `levels` and check it to filter the `push`ed anchors
+    console.log(node, node.children)
     options.anchors.push({
-      title: node.children[0].value,
+      title: node.children.length ? node.children[0].value : '',
       id: node.data.id
     });
   }
