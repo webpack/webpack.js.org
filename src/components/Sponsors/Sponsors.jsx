42a9b6f2c87a1b08d56b6c07ff9ee2bc230bd786
Greg Venech <greg.venech@gmail.com>
2018-05-14 23:54:55 -0400

    chore(rebuild): use `sticky` positioning for `Sponsors` component

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -2,30 +2,32 @@
 import React from 'react';
 import Link from '../Link/Link';
 
-// Load Styling
-import '../Sponsors/Sponsors.scss';
-
 // Imports for Ad Content
 import AGLogo from '../../assets/ag-grid-logo.png';
 import WebpackIcon from '../../assets/icon-square-small.svg';
 
-export default ({ distanceFromTop, height }) => {
+// Load Styling
+import '../Sponsors/Sponsors.scss';
+
+// Create and export the component
+export default props => {
   return (
-    <Link
-      className="sponsors"
-      to="https://www.ag-grid.com/?utm_source=webpack&utm_medium=banner&utm_campaign=sponsorship">
+    <div className="sponsors">
+      <Link
+        className="sponsors__content"
+        to="https://www.ag-grid.com/?utm_source=webpack&utm_medium=banner&utm_campaign=sponsorship">
+        <img src={ AGLogo } />
+        <img src={ WebpackIcon } style={{ width: '100px' }} />
+        <div style={{
+          marginTop: '1em',
+          fontSize: '2em',
+          textAlign: 'center',
+          color: '#535353'
+        }}>
+          ag-grid is proud to partner with webpack
+        </div>
 
-      <img src={ AGLogo } />
-      <img src={ WebpackIcon } style={{ width: '100px' }} />
-      <div style={{
-        marginTop: '1em',
-        fontSize: '2em',
-        textAlign: 'center',
-        color: '#535353'
-      }}>
-        ag-grid is proud to partner with webpack
-      </div>
-
-    </Link>
+      </Link>
+    </div>
   );
 };
