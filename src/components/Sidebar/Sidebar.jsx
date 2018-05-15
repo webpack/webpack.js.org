19a0a9706a91cac2c16f74b9d2aa4140641e2f89
Greg Venech <greg.venech@gmail.com>
2018-05-14 22:59:20 -0400

    chore(rebuild): simplify sidebar to a stateless component

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,40 +1,48 @@
-import React, { Component } from 'react';
+// Import External Dependencies
+import React from 'react';
+
+// Import Local Components
 import Shield from '../Shield/Shield';
 import SidebarItem from '../SidebarItem/SidebarItem';
+
+// Load Styling
 import '../Sidebar/Sidebar.scss';
 
-export default class Sidebar extends Component {
-  render() {
-    let { className = '', pages, currentPage } = this.props;
-    let group;
+// Create and export the component
+export default ({
+  className = '',
+  pages,
+  currentPage,
+  ...props
+}) => {
+  let group;
 
-    return (
-      <nav className={`sidebar ${className}`}>
-        <div className="sidebar__inner">
-          <a href="https://github.com/webpack/webpack/releases">
-            <Shield content="npm/v/webpack" label="webpack" />
-          </a>
+  return (
+    <nav className={`sidebar ${className}`}>
+      <div className="sidebar__inner">
+        <a href="https://github.com/webpack/webpack/releases">
+          <Shield content="npm/v/webpack" label="webpack" />
+        </a>
 
-          {pages.map((page, index) => {
-            let displayGroup = group !== page.group && page.group !== '-';
-            group = page.group;
+        {pages.map((page, index) => {
+          let displayGroup = group !== page.group && page.group !== '-';
+          group = page.group;
 
-            return (
-              <div key={`sidebar-item-${index}`}>
-                {displayGroup ? <h4 className="sidebar__group">{group}</h4> : null}
+          return (
+            <div key={`sidebar-item-${index}`}>
+              {displayGroup ? <h4 className="sidebar__group">{group}</h4> : null}
 
-                <SidebarItem
-                  index={index}
-                  url={page.url}
-                  title={page.title}
-                  anchors={page.anchors}
-                  currentPage={currentPage}
-                />
-              </div>
-            );
-          })}
-        </div>
-      </nav>
-    );
-  }
-}
+              <SidebarItem
+                index={index}
+                url={page.url}
+                title={page.title}
+                anchors={page.anchors}
+                currentPage={currentPage}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </nav>
+  );
+};
