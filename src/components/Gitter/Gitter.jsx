419921e42e941b6e5fae528dbfa6679130083a40
EugeneHlushko <jhlushko@gmail.com>
2018-05-16 10:15:31 +0300

    fix(gitter) refactor gitter component

--- src/components/Gitter/Gitter.jsx
+++ src/components/Gitter/Gitter.jsx
@@ -7,32 +7,34 @@ import isClient from '../../utilities/is-client';
 // Load Styling
 import '../Gitter/Gitter.scss';
 
+let sidecar = false;
+
 // Create and export component
 export default class Gitter extends React.Component {
-  _sidecar = null
-
-  render() {
-    return (
-      <div className="gitter">
-        <div
-          className="gitter__button"
-          onClick={() => window.gitterSidecar && window.gitterSidecar.toggleChat(true)}>
-          <i className="gitter__icon icon-gitter" />
-        </div>
-      </div>
-    );
-  }
-
   componentDidMount() {
     if (isClient) {
       import('gitter-sidecar').then(Sidecar => {
-        if (!window.gitterSidecar) {
-          window.gitterSidecar = new Sidecar({
+        if (!sidecar) {
+          sidecar = new Sidecar({
             room: 'webpack/webpack',
             activationElement: false
           });
         }
       });
     }
+  }
+
+  handleIconClick = () => sidecar && sidecar.toggleChat(true);
+
+  render() {
+    return (
+      <div className="gitter">
+        <div
+          className="gitter__button"
+          onClick={this.handleIconClick}>
+          <i className="gitter__icon icon-gitter" />
+        </div>
+      </div>
+    );
   }
 }
