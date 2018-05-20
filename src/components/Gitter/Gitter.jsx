652d6937034943c363c1bd7167cddfc183e01fc9
Eugene Hlushko <jhlushko@gmail.com>
2018-05-20 03:11:44 +0300

    chore(rebuild): fix gitter widget duplication (#2133)
    The gitter widget was duplicated everytime the user navigated. With
    this check, the component should only be loaded once.

--- src/components/Gitter/Gitter.jsx
+++ src/components/Gitter/Gitter.jsx
@@ -7,14 +7,16 @@ import isClient from '../../utilities/is-client';
 // Load Styling
 import '../Gitter/Gitter.scss';
 
+let sidecar = null;
+
 // Create and export component
 export default class Gitter extends React.Component {
-  _sidecar = null
-
   render() {
     return (
       <div className="gitter">
-        <div className="gitter__button js-gitter-toggle-chat-button">
+        <div
+          className="gitter__button"
+          onClick={this._handleIconClick}>
           <i className="gitter__icon icon-gitter" />
         </div>
       </div>
@@ -24,11 +26,17 @@ export default class Gitter extends React.Component {
   componentDidMount() {
     if (isClient) {
       import('gitter-sidecar').then(Sidecar => {
-        this._sidecar = new Sidecar({
-          room: 'webpack/webpack',
-          activationElement: false
-        });
+        if (!sidecar) {
+          sidecar = new Sidecar({
+            room: 'webpack/webpack',
+            activationElement: false
+          });
+        }
       });
     }
+  }
+
+  _handleIconClick = () => {
+    sidecar && sidecar.toggleChat(true);
   }
 }
