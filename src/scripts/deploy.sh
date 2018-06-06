f4b83e7b26eb2adbd0d2d224794ff4990e003300
byzyk <bohdan.kh@gmail.com>
2018-06-06 13:53:01 +0400

    add test task to pipeline

--- src/scripts/deploy.sh
+++ src/scripts/deploy.sh
@@ -7,14 +7,17 @@ SOURCE_BRANCH="master"
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build and linting links/prose/js."
-    npm run build
     npm test
+    npm run build
     exit 0
 fi
 
 # Save some useful information
 REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+
+# Run tests
+npm test
 
 # Run our build
 npm run build
