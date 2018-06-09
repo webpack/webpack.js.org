67b74251aad7a8883384384e168a27dae637f2a8
Jere Menichelli <jmenichelli@gmail.com>
2018-06-09 18:34:13 +0200

    refactor: rename files

---
+++ src/utilities/fetch-package-names.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+// ./fetchPackageNames <suffix> > output
+// ./fetchPackageNames "-loader" > output.json
+const GitHubAPI = require('@octokit/rest');
+
+if (require.main === module) {
+  main();
+} else {
+  module.exports = fetchPackageNames;
+}
+
+function main() {
+  const organization = process.argv[2];
+  const suffix = process.argv[3];
+
+  fetchPackageNames({
+      organization: organization,
+      suffix: suffix
+    })
+    .then(data => {
+      console.log(JSON.stringify(data, null, 4));
+    })
+    .catch(error => {
+      return console.error(error);
+    });
+}
+
+function fetchPackageNames(options, cb) {
+  if (typeof options.organization !== 'string') {
+    return console.error('utilities/fetch-package-names: missing organization');
+  }
+
+  if (typeof options.suffix !== 'string') {
+    return console.error('utilities/fetch-package-names: missing suffix');
+  }
+
+  // FIX: this handles only one page
+  return new Promise((resolve, reject) => {
+    const github = new GitHubAPI();
+
+    github.repos.getForOrg({
+      org: options.organization,
+      per_page: 100
+    }, (error, response) => {
+      if (error) {
+        reject(error);
+      }
+
+      const filteredNames = response.data.filter((organization) => {
+        return organization.name.endsWith(options.suffix);
+      });
+
+      resolve(filteredNames);
+    });
+  })
+}
