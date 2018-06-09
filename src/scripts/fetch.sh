4455f26439dfb5c8d8795e7f61a7de525aa08c62
Jere Menichelli <jmenichelli@gmail.com>
2018-06-09 18:33:07 +0200

    refactor: filename and code gardening on utilities

--- src/scripts/fetch.sh
+++ src/scripts/fetch.sh
@@ -2,16 +2,16 @@
 set -e # Exit with nonzero exit code if anything fails
 
 # Fetch webpack-contrib (and various other) loader repositories
- node ./src/utilities/fetchPackageNames.js "webpack-contrib" "-loader" | node ./src/utilities/fetchPackageFiles.js "README.md" "./src/content/loaders"
-node ./src/utilities/fetchPackageNames.js "babel" "babel-loader" | node ./src/utilities/fetchPackageFiles.js "README.md" "./src/content/loaders"
-node ./src/utilities/fetchPackageNames.js "postcss" "postcss-loader" | node ./src/utilities/fetchPackageFiles.js "README.md" "./src/content/loaders"
- node ./src/utilities/fetchPackageNames.js "peerigon" "extract-loader" | node ./src/utilities/fetchPackageFiles.js "README.md" "./src/content/loaders"
+ node ./src/utilities/fetch-package-names.js "webpack-contrib" "-loader" | node ./src/utilities/fetch-package-files.js "README.md" "./src/content/loaders"
+node ./src/utilities/fetch-package-names.js "babel" "babel-loader" | node ./src/utilities/fetch-package-files.js "README.md" "./src/content/loaders"
+node ./src/utilities/fetch-package-names.js "postcss" "postcss-loader" | node ./src/utilities/fetch-package-files.js "README.md" "./src/content/loaders"
+ node ./src/utilities/fetch-package-names.js "peerigon" "extract-loader" | node ./src/utilities/fetch-package-files.js "README.md" "./src/content/loaders"
 
 # Fetch webpack-contrib (and various other) plugin repositories
-node ./src/utilities/fetchPackageNames.js "webpack-contrib" "-webpack-plugin" | node ./src/utilities/fetchPackageFiles.js "README.md" "./src/content/plugins"
+node ./src/utilities/fetch-package-names.js "webpack-contrib" "-webpack-plugin" | node ./src/utilities/fetch-package-files.js "README.md" "./src/content/plugins"
 
 # Fetch sponsors and backers from opencollective
-node ./src/utilities/fetchSupporters.js
+node ./src/utilities/fetch-supporters.js
 
 # Fetch starter kits
-node ./src/utilities/fetchStarterKits.js
+node ./src/utilities/fetch-starter-kits.js
