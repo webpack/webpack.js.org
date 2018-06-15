4b50707c2a6e181853a2141ac9cf4fea86fabb13
Fernando Montoya <montogeek@gmail.com>
2018-06-15 12:43:05 +0200

    test token

--- src/scripts/fetch.sh
+++ src/scripts/fetch.sh
@@ -7,20 +7,17 @@ cp -rf ./src/content/loaders/ ./generated/loaders
 mkdir -p ./generated/plugins
 cp -rf ./src/content/plugins/ ./generated/plugins
 
-export GITHUB_TOKEN = $GITHUB_TOKEN
+source $(dirname $0)/env.sh
 
 # Fetch webpack-contrib (and various other) loader repositories
-node ./src/scripts/fetch_package_names.js "webpack-contrib" "-loader" | node ./src/scripts/fetch_package_files.js "README.md" "./generated/loaders"
-node ./src/scripts/fetch_package_names.js "babel" "babel-loader" | node ./src/scripts/fetch_package_files.js "README.md" "./generated/loaders"
-node ./src/scripts/fetch_package_names.js "postcss" "postcss-loader" | node ./src/scripts/fetch_package_files.js "README.md" "./generated/loaders"
-node ./src/scripts/fetch_package_names.js "peerigon" "extract-loader" | node ./src/scripts/fetch_package_files.js "README.md" "./generated/loaders"
+node ./src/scripts/fetch_package_names.js "webpack-contrib" "-loader"
 
 # Fetch webpack-contrib (and various other) plugin repositories
-node ./src/scripts/fetch_package_names.js "webpack-contrib" "-webpack-plugin" | node ./src/scripts/fetch_package_files.js "README.md" "./generated/plugins"
-node ./src/scripts/fetch_package_names.js "webpack-contrib" "-extract-plugin" | node ./src/scripts/fetch_package_files.js "README.md" "./generated/plugins"
+# node ./src/scripts/fetch_package_names.js "webpack-contrib" "-webpack-plugin" | node ./src/scripts/fetch_package_files.js "README.md" "./generated/plugins"
+# node ./src/scripts/fetch_package_names.js "webpack-contrib" "-extract-plugin" | node ./src/scripts/fetch_package_files.js "README.md" "./generated/plugins"
 
 # Remove deprecated or archived plugins repositories
-rm ./generated/plugins/component-webpack-plugin.json ./generated/plugins/component-webpack-plugin.md
+# rm ./generated/plugins/component-webpack-plugin.json ./generated/plugins/component-webpack-plugin.md
 
 # Fetch sponsors and backers from opencollective
 node ./src/scripts/fetch_supporters.js
