aac48e06e745eda2f5674db5578706e69b39344a
Fernando Montoya <montogeek@gmail.com>
2018-06-27 15:09:51 +0200

    Skip tests

--- src/scripts/deploy.sh
+++ src/scripts/deploy.sh
@@ -7,7 +7,7 @@ SOURCE_BRANCH="master"
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build and linting links/prose/js."
-    yarn test
+    # yarn test
     yarn build
     exit 0
 fi
