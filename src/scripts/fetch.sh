a74bab7c128daad519c89ec12389e75de9d983cf
Fernando Montoya <montogeek@gmail.com>
2018-07-06 12:15:01 +0200

    Rename file

--- src/scripts/fetch.sh
+++ src/scripts/fetch.sh
@@ -8,14 +8,14 @@ mkdir -p ./generated/plugins
 cp -rf ./src/content/plugins/ ./generated/plugins
 
 # Fetch webpack-contrib (and various other) loader repositories
-node ./src/scripts/fetch_package_names.js "webpack-contrib" "-loader" "README.md" "./generated/loaders"
-node ./src/scripts/fetch_package_names.js "babel" "babel-loader" "README.md" "./generated/loaders"
-node ./src/scripts/fetch_package_names.js "postcss" "postcss-loader" "README.md" "./generated/loaders"
-node ./src/scripts/fetch_package_names.js "peerigon" "extract-loader" "README.md" "./generated/loaders"
+node ./src/scripts/fetch_packages.js "webpack-contrib" "-loader" "README.md" "./generated/loaders"
+node ./src/scripts/fetch_packages.js "babel" "babel-loader" "README.md" "./generated/loaders"
+node ./src/scripts/fetch_packages.js "postcss" "postcss-loader" "README.md" "./generated/loaders"
+node ./src/scripts/fetch_packages.js "peerigon" "extract-loader" "README.md" "./generated/loaders"
 
 # Fetch webpack-contrib (and various other) plugin repositories
-node ./src/scripts/fetch_package_names.js "webpack-contrib" "-webpack-plugin" "README.md" "./generated/plugins"
-node ./src/scripts/fetch_package_names.js "webpack-contrib" "-extract-plugin" "README.md" "./generated/plugins"
+node ./src/scripts/fetch_packages.js "webpack-contrib" "-webpack-plugin" "README.md" "./generated/plugins"
+node ./src/scripts/fetch_packages.js "webpack-contrib" "-extract-plugin" "README.md" "./generated/plugins"
 
 # Remove deprecated or archived plugins repositories
 rm ./generated/plugins/component-webpack-plugin.json ./generated/plugins/component-webpack-plugin.md
