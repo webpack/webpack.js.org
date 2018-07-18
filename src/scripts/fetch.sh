71106e50f794ae30c2c632f5c9c0ad55396086ff
Fernando Montoya <montogeek@gmail.com>
2018-07-18 18:34:46 +0200

    infra(site) Refactor fetch script

--- src/scripts/fetch.sh
+++ src/scripts/fetch.sh
@@ -22,24 +22,15 @@ fetchPackages() {
   rm ./generated/plugins/component-webpack-plugin.json ./generated/plugins/component-webpack-plugin.md
 }
 
-fetchSupporters() {
-  # Fetch sponsors and backers from opencollective
-  node ./src/scripts/fetch_supporters.js
-}
-
-fetchStarterKits() {
-  # Fetch starter kits
-  node ./src/scripts/fetch_starter_kits.js
-}
-
-if [ "$TRAVIS_PULL_REQUEST" = "" ]
+if [ "$TRAVIS_PULL_REQUEST" == "" ]
 then
   fetchPackages
-  fetchSupporters
-  fetchStarterKits
 else
   echo "PR running, not fetching packages."
-  # This content is still needed
-  fetchSupporters
-  fetchStarterKits
 fi
+
+# Fetch sponsors and backers from opencollective
+node ./src/scripts/fetch_supporters.js
+
+# Fetch starter kits
+node ./src/scripts/fetch_starter_kits.js
