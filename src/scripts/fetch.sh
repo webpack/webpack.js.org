5ca756791e2aca58c51e9366776db8f634d93a85
Fernando Montoya <montogeek@gmail.com>
2018-07-18 17:19:51 +0200

    infra(site) Avoid fetching packages in PRs

--- src/scripts/fetch.sh
+++ src/scripts/fetch.sh
@@ -7,21 +7,30 @@ cp -rf ./src/content/loaders/ ./generated/loaders
 mkdir -p ./generated/plugins
 cp -rf ./src/content/plugins/ ./generated/plugins
 
-# Fetch webpack-contrib (and various other) loader repositories
-node ./src/scripts/fetch_packages.js "webpack-contrib" "-loader" "README.md" "./generated/loaders"
-node ./src/scripts/fetch_packages.js "babel" "babel-loader" "README.md" "./generated/loaders"
-node ./src/scripts/fetch_packages.js "postcss" "postcss-loader" "README.md" "./generated/loaders"
-node ./src/scripts/fetch_packages.js "peerigon" "extract-loader" "README.md" "./generated/loaders"
+function fetchPackages() {
+  # Fetch webpack-contrib (and various other) loader repositories
+  node ./src/scripts/fetch_packages.js "webpack-contrib" "-loader" "README.md" "./generated/loaders"
+  node ./src/scripts/fetch_packages.js "babel" "babel-loader" "README.md" "./generated/loaders"
+  node ./src/scripts/fetch_packages.js "postcss" "postcss-loader" "README.md" "./generated/loaders"
+  node ./src/scripts/fetch_packages.js "peerigon" "extract-loader" "README.md" "./generated/loaders"
 
-# Fetch webpack-contrib (and various other) plugin repositories
-node ./src/scripts/fetch_packages.js "webpack-contrib" "-webpack-plugin" "README.md" "./generated/plugins"
-node ./src/scripts/fetch_packages.js "webpack-contrib" "-extract-plugin" "README.md" "./generated/plugins"
+  # Fetch webpack-contrib (and various other) plugin repositories
+  node ./src/scripts/fetch_packages.js "webpack-contrib" "-webpack-plugin" "README.md" "./generated/plugins"
+  node ./src/scripts/fetch_packages.js "webpack-contrib" "-extract-plugin" "README.md" "./generated/plugins"
 
-# Remove deprecated or archived plugins repositories
-rm ./generated/plugins/component-webpack-plugin.json ./generated/plugins/component-webpack-plugin.md
+  # Remove deprecated or archived plugins repositories
+  rm ./generated/plugins/component-webpack-plugin.json ./generated/plugins/component-webpack-plugin.md
 
-# Fetch sponsors and backers from opencollective
-node ./src/scripts/fetch_supporters.js
+  # Fetch sponsors and backers from opencollective
+  node ./src/scripts/fetch_supporters.js
 
-# Fetch starter kits
-node ./src/scripts/fetch_starter_kits.js
+  # Fetch starter kits
+  node ./src/scripts/fetch_starter_kits.js
+}
+
+if [ "$TRAVIS_PULL_REQUEST" = "" ]
+then
+  fetchPackages
+else
+  echo "PR running, not fetching packages."
+fi
