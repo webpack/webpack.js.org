972de53769c839b98b1a8ad0271b6c3cb333d68a
Fernando Montoya <montogeek@gmail.com>
2018-07-27 03:25:13 +0200

    infra(site) Copy assets when building

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,6 +5,7 @@ const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const SSGPlugin = require('static-site-generator-webpack-plugin');
 const RedirectWebpackPlugin = require('redirect-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const flattenContentTree = require('./src/utilities/flatten-content-tree');
 const contentTree = require('./src/_content.json');
 
@@ -28,7 +29,11 @@ const prod = {
     }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
-    })
+    }),
+    new CopyWebpackPlugin([{
+      from: './assets/icon-square-small-slack.png',
+      to: './assets/'
+    }])
   ]
 };
 
