1a59d6724faaaf39063dd4328bc13fa24337974c
Fernando Montoya <montogeek@gmail.com>
2018-07-31 18:56:24 +0200

    uh

--- prism.js
+++ prism.js
@@ -43,14 +43,14 @@ function isElement(tree, tag, closeTag = false) {
 }
 
 function getDetails(tree, close = false) {
-  const tag = close ? '</details>' : '<details>'
+  const tag = close ? '</details>' : '<details>';
   if (tree.properties.className.includes('keyword') && tree.children[0].value === tag) {
     return tree;
   }
 }
 
 function getSummary(tree, close = false) {
-  const tag = close ? '</summary>' : '<summary>'
+  const tag = close ? '</summary>' : '<summary>';
   if (tree.properties.className.includes('keyword') && tree.children[0].value === tag) {
     return tree;
   }
@@ -73,6 +73,14 @@ function getChildren(tree, index) {
 // console.log('end', inspect(end))
 
 // console.log('between',inspect(findAllBetween(parent, start, end)))
+
+function isElement(tree) {
+  return tree.type === 'element';
+}
+
+function isKeyword(tree) {
+  return tree.properties.className.includes('keyword');
+}
 
 function attacher({ include, exclude } = {}) {
   return function transformer(tree, file) {
@@ -97,57 +105,130 @@ function attacher({ include, exclude } = {}) {
         const newTree = {
           type: 'root',
           children: code
-        }
+        };
 
-        let detailsStart = code.filter(el => el.type === 'element').find(tr => {
-          return tr.properties.className.includes('keyword') && tr.children[0].value === '<details>'
+        let detailsStart = code
+          .filter(isElement)
+          .filter(isKeyword)
+          .filter(tr => {
+            return tr.children[0].value === '<details>';
+          });
+
+        let detailsEnd = code
+          .filter(isElement)
+          .filter(isKeyword)
+          .filter(tr => {
+            return tr.children[0].value === '</details>';
+          });
+
+        let summaryStart = code
+          .filter(isElement)
+          .filter(isKeyword)
+          .filter(tr => {
+            return tr.children[0].value === '<summary>';
+          });
+
+        let summaryEnd = code
+          .filter(isElement)
+          .filter(isKeyword)
+          .filter(tr => {
+            return tr.children[0].value === '</summary>';
+          });
+
+        const summaryTrees = detailsStart.map((_, i) => {
+          return findAllBetween(newTree, summaryStart[i], summaryEnd[i]);
+        });
+
+        const detailsTree = detailsStart.map((_, i) => {
+          return findAllBetween(newTree, detailsStart[i], detailsEnd[i]);
+        });
+
+        const detailsStartIndexes = detailsStart.map((_, i) => {
+          return newTree.children.indexOf(detailsStart[i]);
+        });
+
+        // newTree.children.splice(detailsStartIndexes[0], detailsTree[0].length + 1);
+        // newTree.children.splice(detailsStartIndexes[1] - (detailsTree[0].length + 1), detailsTree[1].length + 1);
+        // newTree.children.splice(detailsStartIndexes[2] - (detailsTree[0].length + 1 + detailsTree[1].length + 1), detailsTree[2].length + 1);
+
+        let prevCount = 0;
+
+        detailsStart.forEach((_, i) => {
+          prevCount += !detailsTree[i - 1] ? 0 : detailsTree[i - 1].length + 1;
+          newTree.children.splice(detailsStartIndexes[i] - prevCount, detailsTree[i].length + 1)
+        });
+
+        const cleanDetailsTrees = detailsStart.map((_, i) => {
+          return detailsTree[i].splice(summaryTrees[i].length + 3)
         })
 
-        let detailsEnd = code.filter(el => el.type === 'element').find(tr => {
-          return tr.properties.className.includes('keyword') && tr.children[0].value === '</details>'
-        })
-
-        let summaryStart = code.filter(el => el.type === 'element').find(tr => {
-          return tr.properties.className.includes('keyword') && tr.children[0].value === '<summary>'
-        })
-
-        let summaryEnd = code.filter(el => el.type === 'element').find(tr => {
-          return tr.properties.className.includes('keyword') && tr.children[0].value === '</summary>'
-        })
-
-        const summaryTree = findAllBetween(newTree, summaryStart, summaryEnd)
-        const detailsTree = findAllBetween(newTree, detailsStart, detailsEnd)
-
-        const dSIndex = newTree.children.indexOf(detailsStart)
-        const dEIndex = newTree.children.indexOf(detailsEnd)
-
-        const sSIndex = newTree.children.indexOf(summaryStart)
-        const sEIndex = newTree.children.indexOf(summaryEnd)
-
-        newTree.children.splice(dSIndex, detailsTree.length + 1)
-
-        const cleanDetailsTree = detailsTree.splice(summaryTree.length + 3)
-
-        const newDetailsTree = {
-          type: 'element',
-          tagName: 'details',
-          children: [
-            {
-              type: 'element',
-              tagName: 'summary',
-              children: summaryTree.splice(1)
+        const newDetailsTrees = detailsStart.map((_, i) => {
+          return {
+            type: 'element',
+            tagName: 'details',
+            properties: {
+              open: false
             },
-            ...cleanDetailsTree
-          ]
-        }
+            children: [
+              {
+                type: 'element',
+                tagName: 'summary',
+                children: summaryTrees[i].splice(1)
+              },
+              ...cleanDetailsTrees[i]
+            ]
+          }
+        })
 
-        newTree.children.splice(dSIndex, 0, newDetailsTree)
+        let nextCount = 0;
 
-        data.hChildren = newTree.children
+        console.log(detailsStartIndexes)
 
+        newDetailsTrees.forEach(e => {
+          console.log(e.children.length + 3)
+        })
+
+        // detailsStart.forEach((_, i) => {
+          newTree.children.splice(detailsStartIndexes[0], 0, newDetailsTrees[0])
+          const prev = detailsStartIndexes[1] - (detailsStartIndexes[0] + newDetailsTrees[0].children.length + 4)
+          newTree.children.splice(detailsStartIndexes[1] - (detailsStartIndexes[0] + newDetailsTrees[0].children.length + 4), 0, newDetailsTrees[1])
+          newTree.children.splice(detailsStartIndexes[2] - (prev + detailsStartIndexes[0]), 0, newDetailsTrees[2])
+        // })
+
+        // console.log(util.inspect(newTree.children.map((e, i) => [i, e]), { depth: 4 }));
+
+        // const summaryTree = findAllBetween(newTree, summaryStart, summaryEnd)
+        // const detailsTree = findAllBetween(newTree, detailsStart, detailsEnd)
+
+        // const dSIndex = newTree.children.indexOf(detailsStart)
+        // const dEIndex = newTree.children.indexOf(detailsEnd)
+
+        // const sSIndex = newTree.children.indexOf(summaryStart)
+        // const sEIndex = newTree.children.indexOf(summaryEnd)
+
+        // newTree.children.splice(dSIndex, detailsTree.length + 1)
+
+        // const cleanDetailsTree = detailsTree.splice(summaryTree.length + 3)
+
+        // const newDetailsTree = {
+        //   type: 'element',
+        //   tagName: 'details',
+        //   children: [
+        //     {
+        //       type: 'element',
+        //       tagName: 'summary',
+        //       children: summaryTree.splice(1)
+        //     },
+        //     ...cleanDetailsTree
+        //   ]
+        // }
+
+        // newTree.children.splice(dSIndex, 0, newDetailsTree)
+
+        data.hChildren = newTree.children;
       } catch (e) {
-        console.log(e)
-        throw e
+        console.log(e);
+        throw e;
       }
 
       data.hProperties = data.hProperties || {};
