20371deef5d9e8c999e559c4afb863ec8ff16008
Peter Müller <munter@fumle.dk>
2018-08-18 01:45:37 +0200

    Change absolute docs links the root relative ones and also resolve some redirects for READMEs that are unchangeable

--- src/utilities/process-readme.js
+++ src/utilities/process-readme.js
@@ -1,5 +1,11 @@
 const url = require('url');
 
+const beginsWithDocsDomainRegex = /^https?:\/\/webpack\.js\.org/;
+
+const redirects = {
+  '/guides/migrating/#json-loader-is-not-required-anymore': '/migrate/3#json-loader-is-not-required-anymore'
+};
+
 module.exports = function processREADME(body, options = {}) {
   return body
     .replace(/[^]*?<div align="center">([^]*?)<\/div>/, (match, content) => {
@@ -14,11 +20,30 @@ module.exports = function processREADME(body, options = {}) {
     // EXAMPLE: [Contributing](./.github/CONTRIBUTING.md)
     // EXAMPLE: [Contributing](CONTRIBUTING.md)
     .replace(/\[([^\]]*)\]\(([^)]+)\)/g, (markdownLink, content, href) => {
+      const oldHref = href;
+
       if (href.includes('//npmjs.com')) {
         href = href.replace('//www.npmjs.com');
       }
 
-      return `[${content}](${url.resolve(options.source, href)})`;
+      if (!href.startsWith('#')) {
+        href = url.resolve(options.source, href);
+      }
+
+      // Modify absolute documenation links to be root relative
+      if (beginsWithDocsDomainRegex.test(href)) {
+        href = href.replace(beginsWithDocsDomainRegex, '');
+      }
+
+      if (href in redirects) {
+        href = redirects[href];
+      }
+
+      if (oldHref !== href) {
+        console.log('REWRITE URL:', oldHref, '-->', href);
+      }
+
+      return `[${content}](${href})`;
     })
     // Modify links to keep them within the site
     .replace(/https?:\/\/github.com\/(webpack|webpack-contrib)\/([-A-za-z0-9]+-loader\/?)([)"])/g, '/loaders/$2/$3')
