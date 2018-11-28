9c16fbacad497a216df5c622a07f822fc8c91cad
lizhihua <275091674@qq.com>
2018-11-28 22:31:08 +0800

    delete copyfiles.sh

--- src/scripts/copyfiles.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e # Exit with nonzero exit code if anything fails
-
-rm -rf ./generated
-mkdir -p ./generated/loaders
-cp -rf ./src/content/loaders/ ./generated/loaders
-mkdir -p ./generated/plugins
-cp -rf ./src/content/plugins/ ./generated/plugins
