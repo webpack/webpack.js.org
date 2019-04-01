545146335ae610afeade08019c0d42dcd833aa42
Anshuman Verma <anshu.av97@gmail.com>
2019-04-01 21:57:33 +0530

    update

--- src/components/Scaffolding/Scaffolding.jsx
+++ src/components/Scaffolding/Scaffolding.jsx
@@ -17,51 +17,50 @@ function htmlDecode (input) {
   //eslint-disable-next-line
   return input.replace(/[^a-z0-9\s]/gi, '').replace(/(gt)/gi, '');
 }
-  const Hit = ({ hit }) => {
-    return (
-      <article className="hit-card">
-        {
-          hit && (
-            <a target="_blank" rel="noopener noreferrer"
-              href={hit.repository && hit.repository.url ? hit.repository.url : `https://npmjs.com/package/${hit.name}`}>
-              <div className="product-wrapper">
-                <div className="product-summary">
-                  <div className="product-title">
-                    <h3>{hit.name}</h3> 
-                  </div>
-                  <div className="product-avatar">
-                    <img src={hit.owner.avatar} height="40" width="40"/>
-                  </div>
+
+const Hit = ({ hit }) => {
+  return (
+    <article className="hit-card">
+      {
+        hit && (
+          <a target="_blank" rel="noopener noreferrer"
+            href={hit.repository && hit.repository.url ? hit.repository.url : `https://npmjs.com/package/${hit.name}`}>
+            <div className="product-wrapper">
+              <div className="product-summary">
+                <div className="product-title">
+                  <h3>{hit.name}</h3> 
                 </div>
-                <div className="product-name">
-                  <h3>{hit.owner.name}</h3>
-                </div>
-                <div className="product-desc">
-                  <p>{ htmlDecode(hit.description) }</p>
+                <div className="product-avatar">
+                  <img src={hit.owner.avatar} height="40" width="40"/>
                 </div>
               </div>
-            </a>
-          ) 
-        }
-      </article>
-    );
-  };
+              <div className="product-name">
+                <h3>{hit.owner.name}</h3>
+              </div>
+              <div className="product-desc">
+                <p>{ htmlDecode(hit.description) }</p>
+              </div>
+            </div>
+          </a>
+        ) 
+      }
+    </article>
+  );
+};
 
 export default class Scaffolding extends React.Component {
   render() {
      return (
-        <div>
-        <div>
-          <InstantSearch searchClient={searchClient} indexName="npm-search">
-            <Configure filters="computedKeywords:webpack-scaffold" hitsPerPage={16} />
-            <div className="search-wrapper">
-              <SearchBox />
-            </div>
-            <div className="hits-wrapper">
-               <Hits hitComponent={Hit} />
-            </div>
-          </InstantSearch>
-        </div>
+      <div>
+        <InstantSearch searchClient={searchClient} indexName="npm-search">
+          <Configure filters="computedKeywords:webpack-scaffold" hitsPerPage={16} />
+          <div className="search-wrapper">
+            <SearchBox />
+          </div>
+          <div className="hits-wrapper">
+              <Hits hitComponent={Hit} />
+          </div>
+        </InstantSearch>
       </div>
     );
   }
