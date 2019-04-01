f21b1cb0868ed4476156c82a19056d7884d4a55f
Anshuman Verma <anshu.av97@gmail.com>
2019-04-01 12:04:00 +0530

    Redirect to npm if gh link is not available

--- src/components/Scaffolding/Scaffolding.jsx
+++ src/components/Scaffolding/Scaffolding.jsx
@@ -22,13 +22,12 @@ import {
     return input.replace(/[^a-z0-9\s]/gi, '').replace(/(gt)/gi, '')
   }
   const Hit = ({ hit }) => {
-  
     return (
       <article className="hit-card">
        {
-          hit.repository && hit.repository.url ? (
+          hit && (
             <a target="_blank" rel="noopener noreferrer"
-              href={hit.repository && hit.repository.url ? hit.repository.url: '/'}>
+              href={hit.repository && hit.repository.url ? hit.repository.url: `https://npmjs.com/package/${hit.name}`}>
               <div className="product-wrapper">
                   <div className="product-summary">
                     <div className="product-title">
@@ -48,27 +47,7 @@ import {
 
               </div>
             </a>
-
-          ) : (
-          <div className="product-wrapper">
-            <div className="product-summary">
-              <div className="product-title">
-                <h3>{hit.name}</h3>
-              </div>
-              <div className="product-avatar">
-                <img src={hit.owner.avatar} height="40" width="40" />
-              </div>
-            </div>
-            <div className="product-name">
-              <h3>{hit.owner.name}</h3>
-            </div>
-
-            <div className="product-desc">
-              <p>{htmlDecode(hit.description)}</p>
-            </div>
-
-        </div>
-          )
+          ) 
        }
       </article>
     );
