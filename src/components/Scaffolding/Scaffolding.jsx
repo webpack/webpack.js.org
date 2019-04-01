19ac54641398fac2452d57c1ad9fa8cae34ea1bd
Anshuman Verma <anshu.av97@gmail.com>
2019-04-01 21:19:31 +0530

    fix indents, general improvements

--- src/components/Scaffolding/Scaffolding.jsx
+++ src/components/Scaffolding/Scaffolding.jsx
@@ -1,8 +1,4 @@
-
 import React from 'react';
-
-import './Scaffolding.scss';
-
 import algoliasearch from 'algoliasearch/lite';
 import {
   InstantSearch,
@@ -10,45 +6,43 @@ import {
   SearchBox,
   Configure,
 } from 'react-instantsearch-dom';
+import './Scaffolding.scss';
 
+const searchClient = algoliasearch(
+  'OFCNCOG2CU',
+  '77db8c6516725ef60b8a60223155f7ad'
+);
 
- const searchClient = algoliasearch(
-    'OFCNCOG2CU',
-    '77db8c6516725ef60b8a60223155f7ad'
-  );
-
-  function htmlDecode(input){
-    //eslint-disable-next-line
-    return input.replace(/[^a-z0-9\s]/gi, '').replace(/(gt)/gi, '')
-  }
+function htmlDecode (input) {
+  //eslint-disable-next-line
+  return input.replace(/[^a-z0-9\s]/gi, '').replace(/(gt)/gi, '');
+}
   const Hit = ({ hit }) => {
     return (
       <article className="hit-card">
-       {
+        {
           hit && (
             <a target="_blank" rel="noopener noreferrer"
-              href={hit.repository && hit.repository.url ? hit.repository.url: `https://npmjs.com/package/${hit.name}`}>
+              href={hit.repository && hit.repository.url ? hit.repository.url : `https://npmjs.com/package/${hit.name}`}>
               <div className="product-wrapper">
-                  <div className="product-summary">
-                    <div className="product-title">
+                <div className="product-summary">
+                  <div className="product-title">
                     <h3>{hit.name}</h3> 
-                    </div>
-                    <div className="product-avatar">
-                      <img src={hit.owner.avatar} height="40" width="40"/>
-                    </div>
                   </div>
-                  <div className="product-name">
+                  <div className="product-avatar">
+                    <img src={hit.owner.avatar} height="40" width="40"/>
+                  </div>
+                </div>
+                <div className="product-name">
                   <h3>{hit.owner.name}</h3>
-                  </div>
-                  
-                  <div className="product-desc">
-                    <p>{htmlDecode(hit.description)}</p>
-                  </div>
-
+                </div>
+                <div className="product-desc">
+                  <p>{ htmlDecode(hit.description) }</p>
+                </div>
               </div>
             </a>
           ) 
-       }
+        }
       </article>
     );
   };
