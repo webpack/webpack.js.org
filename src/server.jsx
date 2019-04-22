dff8955f03db44a4ba8e1ce18375ec99bf76fc59
EugeneHlushko <jhlushko@gmail.com>
2019-04-22 14:14:49 +0300

    misc(renderer) add doctype to our html

--- src/server.jsx
+++ src/server.jsx
@@ -26,7 +26,7 @@ export default locals => {
   let title = getPageTitle(locals.content, locals.path);
   let description = 'webpack is a module bundler. Its main purpose is to bundle JavaScript files for usage in a browser, yet it is also capable of transforming, bundling, or packaging just about any resource or asset.';
 
-  return ReactDOMServer.renderToString(
+  const renderedHtml = ReactDOMServer.renderToString(
     <StaticRouter location={locals.path} context={{}}>
       <html lang="en">
         <head>
@@ -64,4 +64,6 @@ export default locals => {
       </html>
     </StaticRouter>
   );
+
+  return '<!DOCTYPE html>' + renderedHtml;
 };
