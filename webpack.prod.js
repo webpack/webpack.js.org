0b11afd3856a041edf4a9461d53f4efe6c38bfdf
Eugene Hlushko <jhlushko@gmail.com>
2019-04-26 16:33:07 +0300

    feat(app): Add website manifest, minify CSS for production (#2970)

--- webpack.prod.js
+++ webpack.prod.js
@@ -25,6 +25,10 @@ const prod = {
   plugins: [
     new CopyWebpackPlugin([
       {
+        from: './assets/PWA',
+        to: './'
+      },
+      {
         from: './assets/icon-square-small-slack.png',
         to: './assets/'
       },
