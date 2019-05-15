397a205b64a948950b538b6baac628296b43b1e8
Pranshu Chittora <pranshuchittora17@gmail.com>
2019-05-15 14:48:57 +0530

    feat(UX): improves page not found
    improves the 404 Page Not Found to something with better UX

---
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// Styles
+import './PageNotFound.scss';
+
+export default () => {
+  return (
+    <div className="pagenotfound-container">
+      <h1 className="pagenotfound-container__head">Page Not Found</h1>
+      <h4 className="pagenotfound-container__text">
+        Oops! The page you are looking for has been removed or relocated.
+      </h4>
+      <div className="pagenotfound-container__button">
+        <Link to="./">Go Back</Link>
+      </div>
+    </div>
+  );
+};
