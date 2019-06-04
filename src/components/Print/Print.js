40f212873b9d93a1fe2d4f599c1cbb6c3405d9d5
EugeneHlushko <jhlushko@gmail.com>
2019-06-04 19:56:59 +0300

    feat(printable) dont draw printer icon on non-printable sections

--- src/components/Print/Print.js
+++ src/components/Print/Print.js
@@ -2,8 +2,26 @@ import React from 'react';
 
 import icon from '../../assets/icon-print.svg';
 
+const PRINTABLE_SECTIONS = [
+  'api',
+  'concepts',
+  'configuration',
+  'contribute',
+  'guides',
+  'loaders',
+  'migrate',
+  'plugins'
+];
+
 export default function Print (props) {
     const { url } = props;
+    const printUrl = _printPageUrlFromUrl(url);
+
+    // not in a printable section
+    if (!url) {
+      return null;
+    }
+
     return (
       <a href={_printPageUrlFromUrl(url)}
         rel="nofollow"
@@ -20,5 +38,10 @@ function _printPageUrlFromUrl(urlRaw) {
     // when https://github.com/webpack/webpack.js.org/pull/3064 is merged, this is simplified.
     let url = urlRaw[urlRaw.length-1] === '/' ? urlRaw.substring(0, urlRaw.length-1) : urlRaw;
     let urlSplit = url.split('/');
+
+    if (!PRINTABLE_SECTIONS.includes(urlSplit[1])) {
+      return false;
+    }
+
     return (urlSplit.length > 2) ? `/${url.split('/')[1]}/printable/` : `${url}/printable/`;
   }
