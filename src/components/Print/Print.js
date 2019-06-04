a01e1b84115c910df08be44fc2426d6eeaef2574
EugeneHlushko <jhlushko@gmail.com>
2019-06-04 19:51:54 +0300

    feat(printable) hand drawn icon for print

--- src/components/Print/Print.js
+++ src/components/Print/Print.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const imageString = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAH6SURBVGhD7dg/KIRxHMfxQwZK/mRRLEKyGGySUgxSlFISg6wmKwalxGQQ2ZRBKAYGpchmMxuEgYVBUfLf+3vnp6fzex7Pee75p9+nXnXPc7+7ez539z3PI2ES4xTjI0C78CX/rshlcsu/dMEUcRNTJMOYIm5jimQYU8RtTJEMk7UihajWaIS8wJVlnx9GIK9zYNlnVYUc2Ebe8U28QJ4oym4xDG1WIIvucRZhF3j/0owfucMbKpJb0c4k5E2fTW6lRe6QMnHIAOR4F5NbaYlqkUrkp25+J1ZF8rADOa5r1EMlVkVaIMekWA86VkVq8QpVZBwqsZuRQRxiHvKHWiWWw66LKRK1eCoiP4dNAaqBXTwVUafxQXE6jc9KkUfs++gEgRSJwoWVKSKxK1KHdg/aUASVUIqU4Amy34tVqIRSJBcL0A2tW3vohornInLp+JC6qU2UZmQItkVOIXdOQPdd7oGuSAN0652UwS6qyDF0j+3EEWTNGH6kH/KpyAIn1iKleIZunZMt2EUV+c05ZEa1acUSNjS2IU+QPiNz0K13Yp2J9KgiclWoe+w6puD0qTomSjPiKXZF5ASvz4NelEMllCIyI9bL0b9ag0ooReR/sNPQfZfdkhIdUAntq5XtmCJuo4rcQDe02TKDQIoExbciBVgO0ChMYppE4hPrPjmzY7UEnQAAAABJRU5ErkJggg==';
+import icon from '../../assets/icon-print.svg';
 
 export default function Print (props) {
     const { url } = props;
@@ -10,7 +10,7 @@ export default function Print (props) {
         alt="Print"
         title="Print"
         target="_blank">
-        <img src={imageString} />
+        <img src={icon} />
       </a>
     );
 }
