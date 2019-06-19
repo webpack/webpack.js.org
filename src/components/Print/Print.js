76220ad99348a65969a64a7ff2c71c8c453176c4
Eugene Hlushko <jhlushko@gmail.com>
2019-06-19 09:06:52 +0300

    site(ux) Fix the menu issue caused by printable sidebar item index switch (#3131)

--- src/components/Print/Print.js
+++ src/components/Print/Print.js
@@ -15,38 +15,31 @@ const PRINTABLE_SECTIONS = [
 ];
 
 export default function Print (props) {
-    const { url } = props;
-    const printUrl = _printPageUrlFromUrl(url);
+  const { url } = props;
+  const printUrl = _printPageUrlFromUrl(url);
 
-    // not in a printable section
-    if (!url) {
-      return null;
-    }
+  // not in a printable section
+  if (!printUrl) {
+    return null;
+  }
 
-    return (
-      <div className="sidebar-item sidebar-item--disabled`">
-        <i className="sidebar-item__toggle icon-vertical-bar" />
-        <a class="sidebar-item__title sidebar-link__print" href={_printPageUrlFromUrl(url)}
-          rel="nofollow"
-          alt="Print"
-          title="Print"
-          target="_blank">
-          Print Section
-          <img src={icon} />
-        </a>
-      </div>
-    );
+  return (
+    <div className="sidebar-item sidebar-item--disabled`">
+      <i className="sidebar-item__toggle icon-vertical-bar" />
+      <a className="sidebar-item__title sidebar-link__print"
+        href={printUrl}
+        rel="nofollow"
+        alt="Print"
+        title="Print"
+        target="_blank">
+        Print Section
+        <img src={icon} />
+      </a>
+    </div>
+  );
 }
 
 function _printPageUrlFromUrl(urlRaw) {
-    // for now support both trailing slash and without it
-    // when https://github.com/webpack/webpack.js.org/pull/3064 is merged, this is simplified.
-    let url = urlRaw[urlRaw.length-1] === '/' ? urlRaw.substring(0, urlRaw.length-1) : urlRaw;
-    let urlSplit = url.split('/');
-
-    if (!PRINTABLE_SECTIONS.includes(urlSplit[1])) {
-      return false;
-    }
-
-    return (urlSplit.length > 2) ? `/${url.split('/')[1]}/printable/` : `${url}/printable/`;
-  }
+  let urlSplit = urlRaw.split('/');
+  return PRINTABLE_SECTIONS.includes(urlSplit[1]) ? `/${urlSplit[1]}/printable/` : false;
+}
