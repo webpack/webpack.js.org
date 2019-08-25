a15c0cb95e3e047835e6480905569b8468d86453
Peter Müller <munter@fumle.dk>
2019-08-25 19:11:48 +0200

    Run external link definitions in markdown through link replacements and lowercase all fragment identifiers to match markdown generator

--- src/utilities/process-readme.js
+++ src/utilities/process-readme.js
@@ -1,6 +1,8 @@
 const url = require('url');
 
-const beginsWithDocsDomainRegex = /^https?:\/\/webpack\.js\.org/;
+const beginsWithDocsDomainRegex = /^(?:https?:)\/\/webpack\.js\.org/;
+const inlineLinkRegex = /\[[^\]]*\]\(([^)]+)\)/g;
+const externalLinkRegex = /^[ \t]*\[[^\]]+\]:[ \t]+([^ \n]+)/gm;
 
 const fragmentLinkMap = {
   '/api/module-variables/#__webpack_public_path__-webpack-specific-': '/api/module-variables/#__webpack_public_path__-webpack-specific',
@@ -17,6 +19,53 @@ const fragmentLinkMap = {
   '/guides/shimming/#provideplugin': '/plugins/provide-plugin/',
 };
 
+function linkFixerFactory(sourceUrl) {
+  return function linkFixer(markdownLink, href) {
+    const oldHref = href;
+
+    if (href.includes('//npmjs.com')) {
+      href = href.replace('//www.npmjs.com');
+    }
+
+    // Only resolve non-absolute urls from their source if they are not a document fragment link
+    if (!href.startsWith('#')) {
+      // Convert Github raw links to rendered links
+      let rendered_url = sourceUrl
+        .replace(/raw.githubusercontent.com/, 'github.com')
+        .replace(/master/, 'blob/master');
+
+      href = url.resolve(rendered_url, href);
+    }
+
+    // Modify absolute documenation links to be root relative
+    if (beginsWithDocsDomainRegex.test(href)) {
+      href = href.replace(beginsWithDocsDomainRegex, '');
+    }
+
+    const fragmentLinkMapMatch = Object.keys(fragmentLinkMap).find(mapFrom => href.includes(mapFrom));
+    if (fragmentLinkMapMatch) {
+      href = href.replace(fragmentLinkMapMatch, fragmentLinkMap[fragmentLinkMapMatch]);
+      console.error(`DEPRECATED EXTERNAL README LINK:
+  URL: ${sourceUrl}
+  ACTUAL: ${oldHref}
+  EXPECTED: ${oldHref.replace(fragmentLinkMapMatch, fragmentLinkMap[fragmentLinkMapMatch])}`);
+    }
+
+    // Lowercase all fragment links, since markdown generstors do the same
+    if (href.includes('#')) {
+      const [urlPath, urlFragment] = href.split('#');
+
+      href = `${urlPath}#${urlFragment.toLowerCase()}`;
+    }
+
+    if (oldHref !== href) {
+      console.log('REWRITE URL:', oldHref, '-->', href);
+    }
+
+    return markdownLink.replace(oldHref, href);
+  };
+}
+
 module.exports = function processREADME(body, options = {}) {
   return body
     .replace(/[^]*?<div align="center">([^]*?)<\/div>/, (match, content) => {
@@ -30,43 +79,9 @@ module.exports = function processREADME(body, options = {}) {
     // Replace local github links with absolute links to the github location
     // EXAMPLE: [Contributing](./.github/CONTRIBUTING.md)
     // EXAMPLE: [Contributing](CONTRIBUTING.md)
-    .replace(/\[([^\]]*)\]\(([^)]+)\)/g, (markdownLink, content, href) => {
-      const oldHref = href;
-
-      if (href.includes('//npmjs.com')) {
-        href = href.replace('//www.npmjs.com');
-      }
-
-      // Only resolve non-absolute urls from their source if they are not a document fragment link
-      if (!href.startsWith('#')) {
-        // Convert Github raw links to rendered links
-        let rendered_url = options.source
-          .replace(/raw.githubusercontent.com/, 'github.com')
-          .replace(/master/, 'blob/master');
-
-        href = url.resolve(rendered_url, href);
-      }
-
-      // Modify absolute documenation links to be root relative
-      if (beginsWithDocsDomainRegex.test(href)) {
-        href = href.replace(beginsWithDocsDomainRegex, '');
-      }
-
-      const fragmentLinkMapMatch = Object.keys(fragmentLinkMap).find(mapFrom => href.includes(mapFrom));
-      if (fragmentLinkMapMatch) {
-        href = href.replace(fragmentLinkMapMatch, fragmentLinkMap[fragmentLinkMapMatch]);
-        console.error(`DEPRECATED EXTERNAL README LINK:
-  URL: ${options.source}
-  ACTUAL: ${oldHref}
-  EXPECTED: ${oldHref.replace(fragmentLinkMapMatch, fragmentLinkMap[fragmentLinkMapMatch])}`);
-      }
-
-      if (oldHref !== href) {
-        console.log('REWRITE URL:', oldHref, '-->', href);
-      }
-
-      return `[${content}](${href})`;
-    })
+    // EXAMPLE: [line-identifier]: https://webpack.js.org/loaders/
+    .replace(inlineLinkRegex, linkFixerFactory(options.source))
+    .replace(externalLinkRegex, linkFixerFactory(options.source))
     // Modify links to keep them within the site
     .replace(/https?:\/\/github.com\/(webpack|webpack-contrib)\/([-A-za-z0-9]+-loader\/?)([)"])/g, '/loaders/$2/$3')
     .replace(/https?:\/\/github.com\/(webpack|webpack-contrib)\/([-A-za-z0-9]+-plugin\/?)([)"])/g, '/plugins/$2/$3')
