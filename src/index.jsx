828890f7fa67a07697a77f40567d7dad6aef55fe
Resynth <resynth1943@tutanota.com>
2020-05-31 12:38:24 +0000

    Remove Google Analytics Router

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
-import { BrowserRouter as AnalyticsRouter } from 'react-g-analytics';
 
 // Import Components
 import Site from './components/Site/Site';
@@ -12,13 +11,12 @@ import isClient from './utilities/is-client';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-const Router = isProduction ? AnalyticsRouter : BrowserRouter;
 const render = isProduction ? ReactDOM.hydrate : ReactDOM.render;
 
 // Client Side Rendering
 if (isClient) {
   render((
-    <Router id="UA-46921629-2">
+    <BrowserRouter>
       <Route
         path="/"
         render={ props => (
@@ -26,6 +24,6 @@ if (isClient) {
             { ...props }
             import={ path => import(`./content/${path}`) } />
         )} />
-    </Router>
+    </BrowserRouter>
   ), document.getElementById('root'));
 }
