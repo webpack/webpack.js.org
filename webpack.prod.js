6120e418e48f02330bfe17f7a0da17a4b45bbf20
chenxsan <chenxsan@gmail.com>
2020-09-14 22:49:22 +0800

    add runtimeCaching

--- webpack.prod.js
+++ webpack.prod.js
@@ -18,7 +18,36 @@ module.exports = env => merge(common(env), {
   },
   plugins: [
     new GenerateSW({
-      swDest: 'sw.js'
+      swDest: 'sw.js',
+      exclude: [/icon_.*\.png/, /printable/, '/robots.txt'],
+      runtimeCaching: [
+        {
+          urlPattern: /https:\/\/fonts\.gstatic\.com/, // cache google fonts for one year
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'google-fonts',
+            expiration: {
+              maxAgeSeconds: 60 * 60 * 24 * 365,
+              maxEntries: 30
+            }
+          }
+        },
+        {
+          urlPattern: /images\.opencollective\.com\/.*avatar\.png/,
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'avatar',
+            expiration: {
+              maxAgeSeconds: 60 * 60 * 24 * 30,
+              maxEntries: 100
+            }
+          }
+        },
+        {
+          urlPattern: /\/$/, // cache html at runtime
+          handler: 'NetworkFirst',
+        },
+      ],
     })
   ]
 });
