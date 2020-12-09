f7f70b3c74c96931c4c7a5cfa6aa61b494dd9878
Sam Chen <chenxsan@gmail.com>
2020-12-09 12:49:01 +0800

    move MDXProvider to Site (#4284)
    * move MDXProvider to Site
    
    * use scss

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,9 @@
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { BrowserRouter as AnalyticsRouter } from 'react-g-analytics';
-import {MDXProvider} from '@mdx-js/react';
 
 // Import Components
 import Site from './components/Site/Site';
-import Badge from './components/Badge/Badge.js';
 
 // Import helpers
 import isClient from './utilities/is-client';
@@ -19,11 +17,6 @@ const render = isProduction ? ReactDOM.hydrate : ReactDOM.render;
 // Client Side Rendering
 if (isClient) {
   render((
-    <MDXProvider components={{
-      Badge: function Comp (props) {
-        return <Badge {...props} />;
-      }
-    }}>
       <Router id="UA-46921629-2">
       <Route
         path="/"
@@ -33,6 +26,5 @@ if (isClient) {
             import={ path => import(`./content/${path}`) } />
         )} />
     </Router>
-    </MDXProvider>
   ), document.getElementById('root'));
 }
