d722f60f29dc90d496ce87ffc9088f1506ae776d
chenxsan <chenxsan@gmail.com>
2021-01-23 10:03:24 +0800

    test

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -108,7 +108,7 @@ class Page extends Component {
         <PageLinks page={rest} />
 
         <Markdown>
-          <h1>{title} will have stale caches then</h1>
+          <h1>{title} will have stale caches then +1</h1>
 
           {contentRender}
 
