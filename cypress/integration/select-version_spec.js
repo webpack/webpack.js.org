e3439baa516e1e05100473929459d2b133fa156d
chenxsan <chenxsan@gmail.com>
2021-04-10 23:02:53 +0800

    fix test

--- cypress/integration/select-version_spec.js
+++ cypress/integration/select-version_spec.js
@@ -1,6 +1,8 @@
 describe('Select the version of documentation', () => {
   it('should go to v4', () => {
     cy.visit('/concepts/');
+    // close notification bar
+    cy.get('.notification-bar__close').click();
     cy.get('select[name="version-selector"]').select('4');
     cy.url().should('include', 'v4.webpack.js.org');
     cy.url().should('include', '/concepts');
