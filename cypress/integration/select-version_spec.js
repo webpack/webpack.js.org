8e94fb103908bd4478761fbbe094beff26a9b6a4
chenxsan <chenxsan@gmail.com>
2021-04-10 22:57:22 +0800

    add e2e test

---
+++ cypress/integration/select-version_spec.js
@@ -0,0 +1,8 @@
+describe('Select the version of documentation', () => {
+  it('should go to v4', () => {
+    cy.visit('/concepts/');
+    cy.get('select[name="version-selector"]').select('4');
+    cy.url().should('include', 'v4.webpack.js.org');
+    cy.url().should('include', '/concepts');
+  });
+});
