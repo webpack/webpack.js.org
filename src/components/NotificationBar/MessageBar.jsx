e3d6c1892483c16612c1bd2e937b9d5193c06286
chenxsan <chenxsan@gmail.com>
2021-06-21 20:48:01 +0800

    update styles

--- src/components/NotificationBar/MessageBar.jsx
+++ src/components/NotificationBar/MessageBar.jsx
@@ -2,31 +2,50 @@ import CloseIcon from '../../styles/icons/cross.svg';
 import PropTypes from 'prop-types';
 import Content from './Notification.mdx';
 import { version, localStorageIsEnabled } from './NotificationBar';
+import { useTransition, animated, config } from 'react-spring';
+import { useEffect, useState } from 'react';
 
 MessageBar.propTypes = {
   onClose: PropTypes.func,
 };
 export default function MessageBar(props) {
+  const [list, setList] = useState([]);
+  const listTransitions = useTransition(list, {
+    config: config.gentle,
+    from: { opacity: 0, transform: 'translate3d(-50%, 0px, 0px)' },
+    enter: { opacity: 1, transform: 'translate3d(0px, 0px, 0px)' },
+    keys: list.map((item, index) => index),
+  });
+  useEffect(() => {
+    setList(['']);
+  }, []);
   const close = () => {
     localStorage.setItem('notification-dismissed', version);
     props.onClose();
   };
   return (
-    <div className="z-50 fixed left-1/2 transform -translate-x-1/2 bottom-[60px] border-2 border-solid border-blue-400 bg-gray-100 w-[300px] max-w-full p-20 shadow md:left-auto md:translate-x-0 md:right-20 md:bottom-20 dark:bg-gray-500">
-      <Content />
-      {localStorageIsEnabled ? (
-        <div
-          role="button"
-          className="absolute bg-gray-100 rounded-full p-10 border-2 border-solid border-blue-400 flex items-center justify-center cursor-pointer left-1/2 transform -translate-x-1/2 bottom-[-45px] h-[40px] w-[40px] md:right-[5px] md:top-[5px] md:translate-x-0 md:left-auto md:w-20 md:h-20 md:p-0 md:border-none dark:bg-gray-500"
-          onClick={close}
+    <>
+      {listTransitions((styles) => (
+        <animated.div
+          className="flex items-center rounded z-50 fixed left-[1px] right-[1px] bottom-[1px] bg-white border-2 border-solid border-gray-700 max-w-full pl-20 py-20 shadow-2xl md:left-20 md:right-auto md:bottom-20 md:max-w-[300px] dark:bg-gray-500"
+          style={styles}
         >
-          <CloseIcon
-            aria-label="Dismiss"
-            className="fill-current text-blue-600 dark:text-white"
-            width={20}
-          />
-        </div>
-      ) : null}
-    </div>
+          <Content />
+          {localStorageIsEnabled ? (
+            <div
+              role="button"
+              className="px-20 self-stretch inline-flex items-center cursor-pointer"
+              onClick={close}
+            >
+              <CloseIcon
+                aria-label="Dismiss"
+                className="fill-current text-gray-300 dark:text-white transform duration-200 hover:text-gray-700"
+                width={25}
+              />
+            </div>
+          ) : null}
+        </animated.div>
+      ))}
+    </>
   );
 }
