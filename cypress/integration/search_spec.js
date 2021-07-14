fb0e8b68b53650f3641dfa93b788258443bc7b56
chenxsan <chenxsan@gmail.com>
2021-07-14 13:16:18 +0800

    fix e2e test

--- cypress/integration/search_spec.js
+++ cypress/integration/search_spec.js
@@ -1,8 +1,10 @@
 describe('Search', () => {
   it('should visit entry page', () => {
-    cy.visit('/');
+    cy.visit('/concepts/');
     cy.get('.DocSearch').click();
-    cy.get('.DocSearch-input').type('entry points{enter}');
-    cy.url().should('include', '/concepts/entry-points/');
+    cy.get('#docsearch-input').type('roadmap');
+    cy.wait(100);
+    cy.get('#docsearch-item-0').click();
+    cy.url().should('include', '/blog/2020-12-08-roadmap-2021/');
   });
 });
