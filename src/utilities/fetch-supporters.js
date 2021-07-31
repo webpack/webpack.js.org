9c343b6d53190147c3bec5dc8707a988532e11b0
chenxsan <chenxsan@gmail.com>
2021-07-31 12:20:36 +0800

    sleep longer when fetching supporters

--- src/utilities/fetch-supporters.js
+++ src/utilities/fetch-supporters.js
@@ -102,7 +102,7 @@ const getAllNodes = async (graphqlQuery, getNodes) => {
       return allNodes;
     } else {
       // sleep for a while
-      await new Promise((resolve) => setTimeout(resolve, 100));
+      await new Promise((resolve) => setTimeout(resolve, 650));
     }
   }
 };
