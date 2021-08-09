4abb3a894324416b2d4871dff7a76b6079975cc5
chenxsan <chenxsan@gmail.com>
2021-08-09 08:27:45 +0800

    Revert "rename branding.md"
    This reverts commit 2f58c585442a37665b9703c59848bafa962114da.

--- src/components/BrandingSample.jsx
+++
@@ -1,7 +0,0 @@
-import PropTypes from 'prop-types';
-BrandingSample.propTypes = {
-  color: PropTypes.string.isRequired,
-};
-export default function BrandingSample({ color }) {
-  return <div style={{ backgroundColor: color }}>&nbsp;</div>;
-}
