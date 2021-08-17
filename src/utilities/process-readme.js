40ba6913f13f0e03a48f8c502211276db3b2acd1
chenxsan <chenxsan@gmail.com>
2021-08-17 19:37:40 +0800

    Revert "fix regular expression to contain nested div[align="center"]"
    This reverts commit 557e4e930c0fdc66b0f5e5a7d8d70c000a694ae8.

--- src/utilities/process-readme.js
+++ src/utilities/process-readme.js
@@ -94,7 +94,7 @@ module.exports = function processREADME(body, options = {}) {
   let processingString = body
     // remove items other than paragraphs in div[align="center"]
     // e.g., logo, title and so on.
-    .replace(/[^]*?<div align="center">([^]*)<\/div>/, (match, content) => {
+    .replace(/[^]*?<div align="center">([^]*?)<\/div>/, (match, content) => {
       let parsed = content.match(/<p>\s+([^]*?)\s+<\/?p>/);
       return parsed ? parsed[1] : '';
     })
