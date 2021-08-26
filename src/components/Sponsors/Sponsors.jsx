1702d8214442dc192d19b0552302efe3b3fa26b2
jacob-lcs <jacob.lcs@qq.com>
2021-08-26 17:47:28 +0800

    docs(en): fix conflicts

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -10,30 +10,8 @@ import '../Sponsors/Sponsors.scss';
 const Sponsors = () => {
   return (
     <div className="sponsors">
-<<<<<<< HEAD
       <Link className="sponsors__content" to="https://docschina.org">
         <img className="sponsors__img__docschina" src={DocschinaLogo} />
-=======
-      <Link
-        className="sponsors__content"
-        to="https://www.ag-grid.com/?utm_source=webpack&utm_medium=banner&utm_campaign=sponsorship"
-      >
-        <img
-          src={AGLogo}
-          width={260}
-          height={140}
-          alt="ag grid"
-          loading="lazy"
-        />
-        <img
-          className="sponsors__img__webpack"
-          src={WebpackIcon}
-          alt="webpack"
-          width={150}
-          height={150}
-          loading="lazy"
-        />
->>>>>>> 4db15f6f03f5ff286192bc41da319b0171baea4e
         <div className="sponsors__content-cont">
           翻译/文档内容有任何问题，请联系我们
         </div>
