7a3554eb8689327e83a16134fa9e08cd16c234a9
chenxsan <chenxsan@gmail.com>
2021-09-02 20:10:59 +0800

    upgrade remark-gfm

--- src/remark-plugins/remark-responsive-table/remark-responsive-table.test.mjs
+++ src/remark-plugins/remark-responsive-table/remark-responsive-table.test.mjs
@@ -1,9 +1,10 @@
 import { remark } from 'remark';
 import remarkHtml from 'remark-html';
 import remarkResponsiveTable from './remark-responsive-table.mjs';
+import remarkGfm from 'remark-gfm';
 describe('responsive table', () => {
   const processor = remark()
-    .use(require('remark-gfm'))
+    .use(remarkGfm)
     .use(remarkResponsiveTable)
     .use(remarkHtml);
   it('should add data-th', () => {
