6c273cc9b6b27ef82dcbc8a9464ba495d18f89c2
chenxsan <chenxsan@gmail.com>
2022-01-21 18:14:34 +0800

    Redirect vote to v4

--- src/components/Site/Site.jsx
+++ src/components/Site/Site.jsx
@@ -249,7 +249,7 @@ function Site(props) {
                 ),
               },
               { content: 'Contribute', url: '/contribute/' },
-              { content: 'Vote', url: '/vote/' },
+              { content: 'Vote', url: 'https://v4.webpack.js.org/vote/' },
               { content: 'Blog', url: '/blog/' },
             ]}
           />
