368fe34479c4183933fe64a812116b7719f126b7
jacob-lcs <jacob.lcs@qq.com>
2022-01-28 11:15:37 +0800

    fix(en): fix conflicts

--- src/components/Site/Site.jsx
+++ src/components/Site/Site.jsx
@@ -268,16 +268,10 @@ function Site(props) {
                   )
                 ),
               },
-<<<<<<< HEAD
               { content: '参与贡献', url: '/contribute/' },
-              { content: '投票', url: '/vote/' },
+              { content: '投票', url: 'https://v4.webpack.docschina.org/vote/' },
               { content: '博客', url: '/blog/' },
               { content: '印记中文', url: 'https://docschina.org' },
-=======
-              { content: 'Contribute', url: '/contribute/' },
-              { content: 'Vote', url: 'https://v4.webpack.js.org/vote/' },
-              { content: 'Blog', url: '/blog/' },
->>>>>>> f36f9469970338874514a30d25bfb9e89d54fdfc
             ]}
           />
         </div>
