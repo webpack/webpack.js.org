cfd76df85a35dba05570878a5b9281f5fe8d08d3
jacob-lcs <jacob.lcs@qq.com>
2022-04-05 21:31:20 +0800

    fix(cn): fix heading id

--- src/remark-plugins/remark-slug/index.mjs
+++ src/remark-plugins/remark-slug/index.mjs
@@ -27,7 +27,15 @@ export default function remarkSlug() {
       );
       let id = props.id;
 
-      id = id ? slugs.slug(String(id), true) : slugs.slug(toString(node));
+      const rawHeader = id ? id : toString(node);
+      const match = /^.+(\s*\$#([a-z0-9\-_]+?)\$\s*)$/.exec(rawHeader);
+      id = match ? match[2] : slugs.slug(rawHeader, true);
+
+      if (match) {
+        // Remove the custom ID part from the text node.
+        const lastNode = node.children[node.children.length - 1];
+        lastNode.value = lastNode.value.replace(match[1], '');
+      }
 
       data.id = id;
       props.id = id;
