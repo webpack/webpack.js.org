894a41d02a27922fc5e77e3912c43faeb9190559
Sam Chen <chenxsan@gmail.com>
2023-03-07 08:28:26 +0800

    upgrade react-spring (#6663)
    * upgrade react-spring
    
    * print process.env.CI
    
    * log
    
    * log
    
    * fix githubAPI
    
    * fix
    
    * fix
    
    * log
    
    * clean
    
    * debug
    
    * upgrade
    
    * Revert "debug"
    
    This reverts commit 700fe00e7f325881752d5e70e9fe26d217687db0.
    
    * add comment

--- src/utilities/githubAPI.mjs
+++ src/utilities/githubAPI.mjs
@@ -2,7 +2,11 @@ import { Octokit as GithubAPI } from '@octokit/rest';
 import { createActionAuth } from '@octokit/auth-action';
 /** @type import('@octokit/rest').Octokit */
 let api;
-if (process.env.CI && process.env.CI === 'true') {
+if (
+  process.env.CI &&
+  process.env.GITHUB_ACTION &&
+  (process.env.CI === 'true' || process.env.CI === '1') // see https://github.com/cypress-io/github-action/blob/9674a20f82e9e45ec75aa66038310b00e2f24657/index.js#L223 for CI === '1'
+) {
   const auth = createActionAuth();
   const authentication = await auth();
   api = new GithubAPI({
