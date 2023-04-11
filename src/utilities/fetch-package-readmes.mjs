0b2ebced2d716dfb30c228311adc4868a3650bea
Sam Chen <chenxsan@gmail.com>
2023-04-11 19:34:06 +0800

    build(deps-dev): bump mkdirp

--- src/utilities/fetch-package-readmes.mjs
+++ src/utilities/fetch-package-readmes.mjs
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
 import { promisify } from 'util';
-import mkdirp from 'mkdirp';
+import { mkdirp } from 'mkdirp';
 import { fileURLToPath } from 'url';
 import api from './githubAPI.mjs';
 
