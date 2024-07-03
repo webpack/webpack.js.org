ef271827aac0c9363d48a5c25c85776a658a065b
Sam Chen <chenxsan@gmail.com>
2024-07-03 15:30:01 +0800

    Revert "remove isClient"
    This reverts commit a3cfe056b4aec904ee23292b01974351383688f3.

--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,7 @@ import App from './App.jsx';
 
 import './styles/tailwind.css';
 // Import helpers
+import isClient from './utilities/is-client';
 import { HelmetProvider } from 'react-helmet-async';
 
 const isProduction = process.env.NODE_ENV === 'production';
@@ -20,9 +21,12 @@ const JSX = () => (
     </HelmetProvider>
   </Router>
 );
-if (isProduction) {
-  hydrateRoot(container, <JSX />);
-} else {
-  const root = createRoot(container);
-  root.render(<JSX />);
+// Client Side Rendering
+if (isClient) {
+  if (isProduction) {
+    hydrateRoot(container, <JSX />);
+  } else {
+    const root = createRoot(container);
+    root.render(<JSX />);
+  }
 }
