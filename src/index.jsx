e89f564f3d53ed81495dd42c62559d5d8148b3ad
Sam Chen <chenxsan@gmail.com>
2024-07-03 14:24:00 +0800

    upgrade react to v18

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 // Import External Dependencies
-import ReactDOM from 'react-dom';
+import { createRoot, hydrateRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import AnalyticsRouter from './AnalyticsRouter.jsx';
 
@@ -9,20 +9,27 @@ import './styles/tailwind.css';
 // Import helpers
 import isClient from './utilities/is-client';
 import { HelmetProvider } from 'react-helmet-async';
+import { StrictMode } from 'react';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
 const Router = isProduction ? AnalyticsRouter : BrowserRouter;
-const render = isProduction ? ReactDOM.hydrate : ReactDOM.render;
-
-// Client Side Rendering
-if (isClient) {
-  render(
+const container = document.getElementById('root');
+const JSX = () => (
+  <StrictMode>
     <Router id="UA-46921629-2">
       <HelmetProvider>
         <App />
       </HelmetProvider>
-    </Router>,
-    document.getElementById('root')
-  );
+    </Router>
+  </StrictMode>
+);
+// Client Side Rendering
+if (isClient) {
+  if (isProduction) {
+    hydrateRoot(container, <JSX />);
+  } else {
+    const root = createRoot(container);
+    root.render(<JSX />);
+  }
 }
