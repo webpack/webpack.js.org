25fefe5ed4d8b53316b3168356cace93595b9a6f
shelly verma <shellyverma1015@gmail.com>
2025-04-20 01:56:54 -0700

    Added Discord link in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -42,6 +42,9 @@ const Footer = () => (
         <Link className="footer__link" to="https://gitter.im/webpack/webpack">
           Gitter
         </Link>
+        <Link className="footer__link" to="https://discord.com/invite/8WWf6F7p">
+          Discord
+        </Link>
         <Link
           className="footer__link"
           to="https://github.com/webpack/webpack/releases"
