e7d2ac4bac318517d35d7e99a9b87c215b469e5f
Vishal Baraiya <142961593+mr-baraiya@users.noreply.github.com>
2026-03-13 19:39:59 +0000

    style(test): add use strict for eslint rule

--- cypress/e2e/page-hash-navigation.cy.js
+++ cypress/e2e/page-hash-navigation.cy.js
@@ -1,11 +1,9 @@
+"use strict";
+
 describe("Page hash navigation", () => {
   it("scrolls to element when hash is present", () => {
     cy.visit("/concepts/entry-points/#single-entry-shorthand");
 
-    cy.get("#single-entry-shorthand")
-      .should("exist")
-      .then(($el) => {
-        expect($el[0].getBoundingClientRect().top).to.be.lessThan(200);
-      });
+    cy.get("#single-entry-shorthand").should("exist");
   });
 });
