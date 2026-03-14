15322e654afdded8dd78352fa4852b8d7cdd06dd
Vishal Baraiya <142961593+mr-baraiya@users.noreply.github.com>
2026-03-14 14:06:25 +0000

    test(Page): add real Cypress test for hash navigation

--- cypress/e2e/page-hash-navigation.cy.js
+++ cypress/e2e/page-hash-navigation.cy.js
@@ -1,11 +1,16 @@
 "use strict";
 
 describe("Page hash navigation", () => {
-  it("scrolls to the hash target element", () => {
+  it("scrolls to the element specified by the hash", () => {
     cy.visit("/guides/getting-started/#basic-setup");
 
-    cy.location("hash").should("include", "basic-setup");
+    cy.location("hash").should("eq", "#basic-setup");
 
-    cy.get("h2, h3", { timeout: 10000 }).should("exist");
+    cy.get("#basic-setup", { timeout: 10000 })
+      .should("exist")
+      .then(($el) => {
+        const rect = $el[0].getBoundingClientRect();
+        expect(rect.top).to.be.lessThan(200);
+      });
   });
 });
