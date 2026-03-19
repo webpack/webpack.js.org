49dec8bdfb6d4299d451f68cbf44b55ddfe04950
Abir Maity <abirmaity44@gmail.com>
2026-03-19 22:12:35 +0530

    No JS logic changes (only CSS class replacement)

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,9 +1,6 @@
 import { Helmet } from "react-helmet-async";
 import { Link } from "react-router-dom";
 
-// Styles
-import "./PageNotFound.scss";
-
 export default function PageNotFound() {
   return (
     <div className="page markdown">
@@ -13,7 +10,10 @@ export default function PageNotFound() {
       </Helmet>
       <h1>Page Not Found</h1>
       <p>Oops! The page you are looking for has been removed or relocated.</p>
-      <Link className="button" to="/">
+      <Link
+        className="no-underline inline-block py-[3px] px-[5px] text-[1.1rem] rounded-[5px] w-auto border border-[#175d96] text-[#175d96] hover:bg-[#175d96] hover:text-white"
+        to="/"
+      >
         Go to Homepage
       </Link>
     </div>
