5c7361c644ca73ec837f75f5aba78bf9c78ddf41
Kukatla Nandakrishna <nandakrishna0794@gmail.com>
2026-03-27 11:46:01 +0530

    migrate SplashViz component to Tailwind CSS
    - Replaced existing SCSS with Tailwind utility classes
    - Removed SplashViz.scss and its imports
    - Kept the layout and responsiveness consistent with the original

--- src/components/SplashViz/SplashViz.jsx
+++ src/components/SplashViz/SplashViz.jsx
@@ -14,8 +14,13 @@ import "./SplashViz.scss";
 export default class SplashViz extends Component {
   render() {
     return (
-      <section className="splash-viz dark:bg-gray-900!">
-        <h1 className="splash-viz__heading">
+      <section className="splash-viz relative grid grid-rows-[auto_1fr] overflow-hidden p-4 pb-16 bg-[#2b3a42] dark:bg-gray-900! 
+        h-[clamp(35rem,calc(100vh-80px),45rem)] 
+        max-lg:h-[clamp(30rem,calc(100vh-80px),35rem)] 
+        max-[425px]:min-h-[clamp(40rem,calc(100vh-80px),50rem)]">
+        
+        <h1 className="splash-viz__heading mt-[80px] row-start-1 row-end-2 text-center font-extralight text-white 
+          text-[2rem] lg:text-[2.5rem] lg:mt-[90px]">
           <span> bundle your</span>
           <TextRotator delay={5000} repeatDelay={5000} maxWidth={110}>
             <span> assets </span>
@@ -24,12 +29,14 @@ export default class SplashViz extends Component {
             <span> styles </span>
           </TextRotator>
         </h1>
+
         <div
-          className="splash-viz__modules"
+          className="splash-viz__modules absolute top-1/2 left-1/2 w-[60vw] min-w-[550px] max-w-[1200px] m-auto -translate-x-1/2 -translate-y-1/2 hidden lg:block row-start-2 row-end-3"
           dangerouslySetInnerHTML={{ __html: HomeSVG.body }}
         ></div>
+
         <Cube
-          className="splash-viz__cube"
+          className="splash-viz__cube absolute inset-0 m-auto z-[1] row-start-2 row-end-3"
           depth={120}
           repeatDelay={5000}
           continuous
